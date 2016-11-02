ed37fc6623fdc380bd2dc68da5a86558c029aaa4
Shankar Kulumani SEAS-Linux <skulumani@gwu.edu>
2016-11-02 16:45:46 -0400

    Add difference checking of mex and mat files

--- polyhedron_potential_test.m
+++ polyhedron_potential_test.m
@@ -11,7 +11,13 @@ asteroid_grav = polyhedron_shape_input(constants);
 state = [1;0.2;0];
 
 
-[U,Ug,Ug_mat, Ulap] = polyhedron_potential(state, asteroid_grav)
-[U,Ug,Ug_mat, Ulap] = polyhedron_potential_mex_1024(state, asteroid_grav)
+[U_mat,Ug_mat,Ug_mat_mat, Ulap_mat] = polyhedron_potential(state, asteroid_grav)
+[U_mex,Ug_mex,Ug_mat_mex, Ulap_mex] = polyhedron_potential_mex_1024(state, asteroid_grav)
 
 
+fprintf('Difference between mex and matlab functions\n')
+
+fprintf('U_mat-U_mex: %5.4f\n',abs(U_mat-U_mex))
+fprintf('Ug_mat-Ug_mex: %5.4f\n',abs(Ug_mat-Ug_mex))
+fprintf('Ug_mat_mat-Ug_mat_mex: %5.4f\n',abs(Ug_mat_mat-Ug_mat_mex))
+fprintf('Ulat_mat-Ulap_mex: %5.4f\n',abs(Ulap_mat-Ulap_mex))
