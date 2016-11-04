9a413c50d5b76a0bc2fdeae529e6ac64805f1568
Shankar Kulumani SEAS-Linux <skulumani@gwu.edu>
2016-11-04 14:13:21 -0400

    Add relative equations of motion

--- dynamics/ast_eoms_body.m
+++ dynamics/ast_eoms_body.m
@@ -6,10 +6,10 @@
 function [state_dot] = ast_eoms_body(t,state,constants)
 
 % unpack the state
-pos = state(1:3); % location of the center of mass in the inertial frame
-vel = state(4:6);% vel of com in inertial frame
-R = reshape(state(7:15),3,3); % sc body frame to inertial frame
-w = state(16:18); % angular velocity of sc wrt inertial frame defined in body frame
+pos = state(1:3); % location of the COM of dumbbell in asteroid fixed frame
+vel = state(4:6);% vel of com wrt to asteroid expressed in the asteroid fixed frame
+R = reshape(state(7:15),3,3); % sc body frame to asteroid body frame
+w = state(16:18); % angular velocity of sc wrt asteroid frame and expressed in asteroid fixed frame
 
 Ra = ROT3(-constants.omega*t); % asteroid body frame to inertial frame
 
@@ -19,16 +19,18 @@ m2 = constants.m2;
 lcg = constants.lcg;
 l = constants.l;
 J = constants.J;
+Jr = R*J*R';
+Omega = constants.omega*[0;0;1]; % angular velocity vector of asteroid
 
 % the position of each mass in the asteroid body frame
 rho_1 = lcg*[1;0;0];
 rho_2= (l-lcg)*[-1;0;0];
 
 % position of each mass in the asteroid frame
-z1 = Ra'*(pos + R * rho_1);
-z2 = Ra'*(pos + R * rho_2);
+z1 = (pos + R * rho_1);
+z2 = (pos + R * rho_2);
 
-z = Ra' * pos; % position of COM in asteroid frame
+z = pos; % position of COM in asteroid frame
 
 switch constants.pot_model
     case 'polyhedron'
@@ -41,6 +43,10 @@ switch constants.pot_model
                 [~,U_grad_m1,~, ~] = polyhedron_potential_mex_4092(z1, constants.asteroid_grav);
                 [~,U_grad_m2,~, ~] = polyhedron_potential_mex_4092(z2, constants.asteroid_grav);
                 [~,U_grad_com,~, ~] = polyhedron_potential_mex_4092(z, constants.asteroid_grav);
+            case 32
+                [~,U_grad_m1,~, ~] = polyhedron_potential_mex_32(z1, constants.asteroid_grav);
+                [~,U_grad_m2,~, ~] = polyhedron_potential_mex_32(z2, constants.asteroid_grav);
+                [~,U_grad_com,~, ~] = polyhedron_potential_mex_32(z, constants.asteroid_grav);
         end
     case 'mascon'
         [~,U_grad_m1] = mascon_potential(z1,constants.asteroid_grav,constants);
@@ -53,25 +59,40 @@ switch constants.pot_model
 end
 
 % force due to each mass expressed in asteroid body frame
-F1 = m1*Ra*U_grad_m1;
-F2 = m2*Ra*U_grad_m2;
-F_com = (m1+m2)*Ra*U_grad_com;
+F1 = m1*U_grad_m1;
+F2 = m2*U_grad_m2;
+F_com = (m1+m2)*U_grad_com;
 
 % compute the moments due to each mass
-M1 = m1*(R'*U_grad_m1*(Ra'*rho_1)' - (Ra'*rho_1)*U_grad_m1'*R);
-M2 = m2*(R'*U_grad_m2*(Ra'*rho_2)' - (Ra'*rho_2)*U_grad_m2'*R);
+M1 = m1*(rho_1*U_grad_m1'*R' - R*U_grad_m1*rho_1');
+M2 = m2*(rho_2*U_grad_m2'*R' - R*U_grad_m2*rho_2');
 
 % apply the vee map to get a vector
 M1 = vee_map(M1);
 M2 = vee_map(M2);
 
 % state derivatives
-pos_dot = vel;
-vel_dot = 1/(m1+m2) * (F1+F2);
+pos_dot = vel - hat_map(Omega)*pos;
+vel_dot = 1/(m1+m2) * (F1 + F2 - (m1+m2) * hat_map(Omega)*vel);
 % vel_dot = 1/(m1+m2) * (F_com) ;
-R_dot = R*hat_map(w);
+R_dot = hat_map(w)*R - hat_map(Omega)*R;
 R_dot = reshape(R_dot,9,1);
-w_dot = J\(M1 + M2 - hat_map(w)*(J*w));
+w_dot = Jr\ (M1 + M2 - hat_map(Omega)*(Jr*w) - (( hat_map(w) - hat_map(Omega))*Jr + Jr*( hat_map(Omega)-hat_map(w) ))*w);
 
 state_dot = [pos_dot;vel_dot;R_dot;w_dot];
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
