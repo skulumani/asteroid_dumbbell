495cd0564d340a6de9d5bdcca7444851277c3290
Shankar Kulumani SEAS-Linux <skulumani@gwu.edu>
2016-11-04 14:15:55 -0400

    Total energy for relative equations of motion

--- plotting/energy_plot_body.m
+++ plotting/energy_plot_body.m
@@ -5,16 +5,21 @@ function [] = energy_plot(t,state_body,constants)
 
 fontsize = constants.fontsize;
 fontname = constants.fontname;
+m1 = constants.m1;
+m2 = constants.m2;
 
-pos_cm = state_body(:,1:3);
-vel_cm = state_body(:,4:6);
-w_ast2sc = state_body(:,16:18);
+pos_cm = state_body(:,1:3);% location of the COM of dumbbell in asteroid fixed frame
+vel_cm = state_body(:,4:6);% vel of com wrt to asteroid expressed in the asteroid fixed frame
+w_sc2ast = state_body(:,16:18);% angular velocity of sc wrt asteroid frame and expressed in asteroid fixed frame
 % extract and store the rotation matrix
-R_ast2sc = zeros(3,3,length(t));
+R_sc2ast = zeros(3,3,length(t));% sc body frame to asteroid body frame
 
 for ii = 1:length(t)
-    R_ast2sc(:,:,ii) = reshape(state_body(ii,7:15),3,3); % asteroid to sc frame
+    R_sc2ast(:,:,ii) = reshape(state_body(ii,7:15),3,3); % sc to asteroid body frame
 end
+
+rho_1 = constants.lcg*[1;0;0];
+rho_2= (constants.l-constants.lcg)*[-1;0;0];
 
 % compute the total energy of the system
 E = zeros(length(t),1);
@@ -23,17 +28,11 @@ V = zeros(length(t),1);
 J = zeros(length(t),1);
 
 for ii = 1:length(t)
-    % compute the position and velocity of COM
     
-    % compute position of each mass in asteroid rotating frame
-    vel_inertial = vel_cm(ii,:)' + hat_map(constants.omega*[0;0;1])*pos_cm(ii,:)';
     
-    pos_m1 = pos_cm(ii,:)' - R_ast2sc(:,:,ii)'*constants.lcg*[1;0;0];
-    pos_m2 = pos_cm(ii,:)' + R_ast2sc(:,:,ii)'*(constants.l - constants.lcg)*[1;0;0];
+    pos_m1 = pos_cm(ii,:)' + R_sc2ast(:,:,ii)*rho_1;
+    pos_m2 = pos_cm(ii,:)' + R_sc2ast(:,:,ii)*rho_2;
   
-    % angular velocity of body wrt inertial frame represented in the
-    % asteroid body fixed frame
-    w_inertial2sc = constants.omega*[0;0;1] + w_ast2sc(ii,:)';
     % potential energy
     switch constants.pot_model
         case 'polyhedron'
@@ -46,6 +45,10 @@ for ii = 1:length(t)
                     [U_m1,~,~, ~] = polyhedron_potential_mex_4092(pos_m1, constants.asteroid_grav);
                     [U_m2,~,~, ~] = polyhedron_potential_mex_4092(pos_m2, constants.asteroid_grav);
                     [U_com,~,~,~] = polyhedron_potential_mex_4092(pos_cm(ii,:)',constants.asteroid_grav);
+                case 32
+                    [U_m1,~,~, ~] = polyhedron_potential_mex_32(pos_m1, constants.asteroid_grav);
+                    [U_m2,~,~, ~] = polyhedron_potential_mex_32(pos_m2, constants.asteroid_grav);
+                    [U_com,~,~,~] = polyhedron_potential_mex_32(pos_cm(ii,:)',constants.asteroid_grav);
             end
         case 'mascon'
             [U_m1,~] = mascon_potential(pos_m1,constants.asteroid_grav,constants);
@@ -57,9 +60,8 @@ for ii = 1:length(t)
             [U_com,~, ~, ~] = polyhedron_potential(pos_cm(ii,:)', constants.asteroid_grav);
     end
     
-%     T(ii) = 1/2*w_inertial2sc'*constants.J*w_inertial2sc + 1/2*(constants.m1+constants.m2)*(vel_inertial'*vel_inertial);
-    T(ii) = 1/2*w_inertial2sc'*constants.J*w_inertial2sc + (constants.m1+constants.m2)*(1/2*(vel_cm(ii,:)*vel_cm(ii,:)') - 1/2*constants.omega^2*(pos_cm(ii,1)^2+pos_cm(ii,2)^2));
-    V(ii) = -constants.m1*U_m1 - constants.m2*U_m2;
+    T(ii) = 1/2*(m1+m2)*vel_cm(ii,:)*vel_cm(ii,:)' + 1/2*w_sc2ast(ii,:)*constants.J*w_sc2ast(ii,:)';
+    V(ii) = -m1*U_m1 - m2*U_m2;
     E(ii) = T(ii) + V(ii);
 
     J(ii) = 1/2*(vel_cm(ii,:)*vel_cm(ii,:)') - 1/2*constants.omega^2*(pos_cm(ii,1)^2+pos_cm(ii,2)^2) - U_com;
