6bda3d282531b6f0d1d74ab8137426008eb4cd1b
Shankar Kulumani SEAS-Linux <skulumani@gwmail.gwu.edu>
2016-11-14 13:24:30 -0500

    Add start of energy behavior function

---
+++ dynamics/inertial_eoms_energy_behavior.m
@@ -0,0 +1,14 @@
+% 14 November 2016
+% plot the energy variation of the inertial equations of motion as a
+% function of the error tolerance of the ODE
+
+% load constants
+
+% loop over a range of error tolerance
+
+% simulate the ODE
+
+% save the state and energy behavior
+
+% plot to the same plot
+
