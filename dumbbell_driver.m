96033d29a2d6ac938e1ab6b60e909b75c2d1d9d8
Shankar Kulumani SEAS-Linux <skulumani@gwmail.gwu.edu>
2016-11-15 17:04:40 -0500

    Testing out EOMS'

--- dumbbell_driver.m
+++ dumbbell_driver.m
@@ -48,23 +48,23 @@ initial_state = [initial_pos;initial_vel;initial_R;initial_w];
 
 fprintf('FINISHED INERTIAL SIM\n')
 
-%% SIMULATE ODE
-fprintf('BODY SIMULATION!\n')
-
-t_step = 0.1;
-t_final = 100000;
-constants.num_steps = t_final/t_step;
-
-tspan = linspace(0,t_final,constants.num_steps);
-initial_pos = [1.495746722510590;0.000001002669660;0.006129720493607]; % km for center of mass
-initial_vel = [0.000000302161724;-0.000899607989820;-0.000000013286327]; % km/sec for COM in asteroid fixed frame
-initial_R = reshape(eye(3,3),9,1); % transforms from dumbbell body frame to asteroid body frame
-initial_w = [0;0;0]; % angular velocity of dumbbell wrt to asteroid represented in asteroid body fixed frame
-
-initial_state = [initial_pos;initial_vel;initial_R;initial_w];
-[t_body,state_body] = ode113(@(t,state)ast_eoms_body(t,state,constants),tspan,initial_state,constants.ode_options);
-
-fprintf('FINISHED BODY SIM\n')
+% %% SIMULATE ODE
+% fprintf('BODY SIMULATION!\n')
+% 
+% t_step = 0.1;
+% t_final = 100000;
+% constants.num_steps = t_final/t_step;
+% 
+% tspan = linspace(0,t_final,constants.num_steps);
+% initial_pos = [1.495746722510590;0.000001002669660;0.006129720493607]; % km for center of mass
+% initial_vel = [0.000000302161724;-0.000899607989820;-0.000000013286327]; % km/sec for COM in asteroid fixed frame
+% initial_R = reshape(eye(3,3),9,1); % transforms from dumbbell body frame to asteroid body frame
+% initial_w = [0;0;0]; % angular velocity of dumbbell wrt to asteroid represented in asteroid body fixed frame
+% 
+% initial_state = [initial_pos;initial_vel;initial_R;initial_w];
+% [t_body,state_body] = ode113(@(t,state)ast_eoms_body(t,state,constants),tspan,initial_state,constants.ode_options);
+% 
+% fprintf('FINISHED BODY SIM\n')
 
 %% SIMULATE ODE
 % fprintf('TRANSLATION SIMULATION!\n')
