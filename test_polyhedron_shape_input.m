64a2f5ca266c7e06a1e448f3da7b8128e13f2b1d
Shankar Kulumani <shanks.k@gmail.com>
2017-02-07 12:54:52 -0500

    Add some testing functions

---
+++ test_polyhedron_shape_input.m
@@ -0,0 +1,12 @@
+% asteroid tester
+
+addpath(genpath(['.',filesep]));
+
+load('/Users/shankar/Drive/GWU/research/asteroid_dumbbell/dynamics/CASTALIA/castalia_model.mat')
+
+asteroid_params.F = F_32;
+asteroid_params.V = V_32;
+asteroid_params.G = 6.673e-20; % km^3/kg/sec^2
+asteroid_params.sigma = 1.9;
+
+asteroid_grav = polyhedron_shape_input(asteroid_params)
