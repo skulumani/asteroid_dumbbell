31d78e8e8c23b9ea04d10a12ed56d9d5a8fdfcf8
Shankar Kulumani <shanks.k@gmail.com>
2017-02-06 23:56:55 -0500

    Converted everything but need to test

--- dynamics/asteroid.py
+++ dynamics/asteroid.py
@@ -112,9 +112,9 @@ class Asteroid(object):
         # calculate shape parameters
         # calculate vectors for each edge (loop through F and difference the
         # vectors) then store them
-        e1_face_map = np.zeros([num_f,4])
-        e2_face_map = np.zeros([num_f,4])
-        e3_face_map = np.zeros([num_f,4])
+        e1_face_map = np.full([num_f,4],-1.0)
+        e2_face_map = np.full([num_f,4],-1.0)
+        e3_face_map = np.full([num_f,4],-1.0)
 
         F_face = np.zeros([3,3,num_f])
 
@@ -166,173 +166,148 @@ class Asteroid(object):
             F_face[:,:,ii] = np.outer(normal_face[ii,:],normal_face[ii,:])
         
         # loop over all the edges to figure out the common edges and calculate E_e
-
+ 
         # find common e1 edges
-        [e1_ind1a,e1_ind1b] = ismember(-e1,e1,'rows');
-        [e1_ind2a,e1_ind2b] = ismember(-e1,e2,'rows');
-        [e1_ind3a,e1_ind3b] = ismember(-e1,e3,'rows');
+        e1_ind1b = utilities.in1d_index(-e1,e1)
+        e1_ind2b = utilities.in1d_index(-e1,e2)
+        e1_ind3b = utilities.in1d_index(-e1,e3)
 
-        [e2_ind1a,e2_ind1b] = ismember(-e2,e1,'rows');
-        [e2_ind2a,e2_ind2b] = ismember(-e2,e2,'rows');
-        [e2_ind3a,e2_ind3b] = ismember(-e2,e3,'rows');
+        e2_ind1b = utilities.in1d_index(-e2,e1)
+        e2_ind2b = utilities.in1d_index(-e2,e2)
+        e2_ind3b = utilities.in1d_index(-e2,e3)
 
-        [e3_ind1a,e3_ind1b] = ismember(-e3,e1,'rows');
-        [e3_ind2a,e3_ind2b] = ismember(-e3,e2,'rows');
-        [e3_ind3a,e3_ind3b] = ismember(-e3,e3,'rows');
+        e3_ind1b = utilities.in1d_index(-e3,e1)
+        e3_ind2b = utilities.in1d_index(-e3,e2)
+        e3_ind3b = utilities.in1d_index(-e3,e3)
 
-        E1_edge = zeros(3,3,num_f);
-        E2_edge = zeros(3,3,num_f);
-        E3_edge = zeros(3,3,num_f);
+        E1_edge = np.zeros([3,3,num_f])
+        E2_edge = np.zeros([3,3,num_f])
+        E3_edge = np.zeros([3,3,num_f])
 
-        for ii = 1:num_f
+        for ii in range(num_f):
+            pdb.set_trace()
+            # check each of the edges in the current face for a match in all the
+            # other edges
+            e1_face_map[ii,0] = ii
+            e2_face_map[ii,0] = ii
+            e3_face_map[ii,0] = ii
 
-        % check each of the edges in the current face for a match in all the
-        % other edges
-        e1_face_map(ii,1) = ii;
-        e2_face_map(ii,1) = ii;
-        e3_face_map(ii,1) = ii;
+            # e1 edge duplicate
+            if ii in e1_ind1b:
+                e1_face_map[ii,1] = ii
+            elif ii in e1_ind2b:
+                e1_face_map[ii,2] = ii
+            elif ii in e1_ind3b:
+                e1_face_map[ii,3] = ii
 
-        % e1 edge duplicate
+            # e2 edge duplicate
+            if ii in e2_ind1b:
+                e2_face_map[ii,1] = ii
+            elif ii in e2_ind2b:
+                e2_face_map[ii,2] = ii
+            elif ii in e2_ind3b:
+                e2_face_map[ii,3] = ii
+            
+            # e3 edge duplicate
+            if ii in e3_ind1b:
+                e3_face_map[ii,1] = ii
+            elif ii in e3_ind2b:
+                e3_face_map[ii,2] = ii
+            elif ii in e3_ind3b:
+                e3_face_map[ii,3] = ii
 
-        ind1 = e1_ind1b(ii);
-        ind2 = e1_ind2b(ii);
-        ind3 = e1_ind3b(ii);
+            # find the edge normals for all edges of the current face
+            # also pull out the edge normals for each adjacent face (3 adjacent
+            # faces
+            nA1 = e1_normal[e1_face_map[ii,0],:]
+            nA2 = e2_normal[e2_face_map[ii,0],:]
+            nA3 = e3_normal[e3_face_map[ii,0],:]
 
-        if ind1 ~= 0
-        e1_face_map(ii,2) = ind1;
-        elseif ind2 ~= 0
-        e1_face_map(ii,3) = ind2;
-        elseif ind3 ~= 0
-        e1_face_map(ii,4) = ind3;
-        end
+            # find adjacent face for edge 1
+            col = np.where(e1_face_map[ii,1:] >= 0)[0]
+            face_index = int(e1_face_map[ii,col+1])
 
-        % e2 edge duplicate
+            if col == 0: # adjacent face is also edge 1
+                nB1 = e1_normal[face_index,:]
+            elif col == 1: # adjacent face is edge 2
+                nB1 = e2_normal[face_index,:] 
+            elif col == 2:
+                nB1 = e3_normal[face_index,:]
 
-        ind1 = e2_ind1b(ii);
-        ind2 = e2_ind2b(ii);
-        ind3 = e2_ind3b(ii);
+            nA = normal_face[ii,:]
+            nB = normal_face[face_index,:]
+            
+            E1_edge[:,:,ii] = np.outer(nA,nA1) + np.outer(nB,nB1) # second order dyadic tensor
 
-        if ind1 ~= 0
-        e2_face_map(ii,2) = ind1;
-        elseif ind2 ~= 0
-        e2_face_map(ii,3) = ind2;
-        elseif ind3 ~= 0
-        e2_face_map(ii,4) = ind3;
-        end
+            # find adjacent face for edge 2
+            col = np.where(e2_face_map[ii,1:] >= 0)[0]
+            face_index = int(e2_face_map[ii,col+1])
 
-        % e3 edge duplicate
+            if col == 0: # adjacent face is also edge 1
+                nB2 = e1_normal[face_index,:] 
+            elif col == 1: # adjacent face is edge 2
+                nB2 = e2_normal[face_index,:] 
+            elif col == 2:
+                nB2 = e3_normal[face_index,:] 
 
-        ind1 = e3_ind1b(ii);
-        ind2 = e3_ind2b(ii);
-        ind3 = e3_ind3b(ii);
+            nB = normal_face[face_index,:]
 
-        if ind1 ~= 0
-        e3_face_map(ii,2) = ind1;
-        elseif ind2 ~= 0
-        e3_face_map(ii,3) = ind2;
-        elseif ind3 ~= 0
-        e3_face_map(ii,4) = ind3;
-        end
+            E2_edge[:,:,ii] = np.outer(nA,nA2) + np.outer(nB,nB2) # second order dyadic tensor
 
-        % find the edge normals for all edges of the current face
-        % also pull out the edge normals for each adjacent face (3 adjacent
-        % faces
-        nA1 = e1_normal(e1_face_map(ii,1),:);
-        nA2 = e2_normal(e2_face_map(ii,1),:);
-        nA3 = e3_normal(e3_face_map(ii,1),:);
+            pdb.set_trace()
+            # find adjacent face for edge 3
+            col = np.where(e3_face_map[ii,1:] >= 0)[0]
+            face_index = int(e3_face_map[ii,col+1])
 
-        % find adjacent face for edge 1
-        col = find(e1_face_map(ii,2:end) ~= 0);
-        face_index = e1_face_map(ii,col+1);
+            if col == 0: # adjacent face is also edge 1
+                nB3 = e1_normal[face_index,:] 
+            elif col == 1: # adjacent face is edge 2
+                nB3 = e2_normal[face_index,:] 
+            elif col == 2:
+                nB3 = e3_normal[face_index,:] 
 
-        if col == 1 % adjacent face is also edge 1
+            nB = normal_face[face_index,:]
 
-        nB1 = e1_normal(face_index,:); 
-        elseif col == 2 % adjacent face is edge 2
-        nB1 = e2_normal(face_index,:); 
-        elseif col == 3
-        nB1 = e3_normal(face_index,:); 
-        end
+            E3_edge[:,:,ii] = np.outer(nA,nA3) + np.outer(nB,nB3) # second order dyadic tensor
 
-        nA = normal_face(ii,:);
-        nB = normal_face(face_index,:);
+            # % save as a structure with all the precomputed polyhedron potential data
 
-        E1_edge(:,:,ii) = nA'*nA1 + nB'*nB1; % second order dyadic tensor
+            # asteroid_grav.F = F;
+            # asteroid_grav.V = V;
 
-        % find adjacent face for edge 2
-        col = find(e2_face_map(ii,2:end) ~= 0);
-        face_index = e2_face_map(ii,col+1);
+            # asteroid_grav.V1 = V1;
+            # asteroid_grav.V2 = V2;
+            # asteroid_grav.V3 = V3;
 
-        if col == 1 % adjacent face is also edge 1
-        nB2 = e1_normal(face_index,:); 
-        elseif col == 2 % adjacent face is edge 2
-        nB2 = e2_normal(face_index,:); 
-        elseif col == 3
-        nB2 = e3_normal(face_index,:); 
-        end
+            # asteroid_grav.e1 = e1;
+            # asteroid_grav.e2 = e2;
+            # asteroid_grav.e3 = e3;
 
+            # asteroid_grav.e1_face_map = e1_face_map;
+            # asteroid_grav.e2_face_map = e2_face_map;
+            # asteroid_grav.e3_face_map = e3_face_map;
 
-        nB = normal_face(face_index,:);
+            # asteroid_grav.e1_vertex_map = e1_vertex_map;
+            # asteroid_grav.e2_vertex_map = e2_vertex_map;
+            # asteroid_grav.e3_vertex_map = e3_vertex_map;
 
-        E2_edge(:,:,ii) = nA'*nA2 + nB'*nB2; % second order dyadic tensor
+            # asteroid_grav.normal_face = normal_face;
+            # asteroid_grav.center_face = center_face;
 
-        % find adjacent face for edge 3
-        col = find(e3_face_map(ii,2:end) ~= 0);
-        face_index = e3_face_map(ii,col+1);
+            # asteroid_grav.e1_normal = e1_normal;
+            # asteroid_grav.e2_normal = e2_normal;
+            # asteroid_grav.e3_normal = e3_normal;
 
-        if col == 1 % adjacent face is also edge 1
-        nB3 = e1_normal(face_index,:); 
-        elseif col == 2 % adjacent face is edge 2
-        nB3 = e2_normal(face_index,:); 
-        elseif col == 3
-        nB3 = e3_normal(face_index,:); 
-        end
+            # asteroid_grav.E1_edge = E1_edge;
+            # asteroid_grav.E2_edge = E2_edge;
+            # asteroid_grav.E3_edge = E3_edge;
 
+            # asteroid_grav.F_face = F_face;
 
-        nB = normal_face(face_index,:);
+            # asteroid_grav.num_f = num_f;
+            # asteroid_grav.num_e = num_e;
+            # asteroid_grav.num_v = num_v;
 
-        E3_edge(:,:,ii) = nA'*nA3 + nB'*nB3; % second order dyadic tensor
-
-        end
-
-        % save as a structure with all the precomputed polyhedron potential data
-
-        asteroid_grav.F = F;
-        asteroid_grav.V = V;
-
-        asteroid_grav.V1 = V1;
-        asteroid_grav.V2 = V2;
-        asteroid_grav.V3 = V3;
-
-        asteroid_grav.e1 = e1;
-        asteroid_grav.e2 = e2;
-        asteroid_grav.e3 = e3;
-
-        asteroid_grav.e1_face_map = e1_face_map;
-        asteroid_grav.e2_face_map = e2_face_map;
-        asteroid_grav.e3_face_map = e3_face_map;
-
-        asteroid_grav.e1_vertex_map = e1_vertex_map;
-        asteroid_grav.e2_vertex_map = e2_vertex_map;
-        asteroid_grav.e3_vertex_map = e3_vertex_map;
-
-        asteroid_grav.normal_face = normal_face;
-        asteroid_grav.center_face = center_face;
-
-        asteroid_grav.e1_normal = e1_normal;
-        asteroid_grav.e2_normal = e2_normal;
-        asteroid_grav.e3_normal = e3_normal;
-
-        asteroid_grav.E1_edge = E1_edge;
-        asteroid_grav.E2_edge = E2_edge;
-        asteroid_grav.E3_edge = E3_edge;
-
-        asteroid_grav.F_face = F_face;
-
-        asteroid_grav.num_f = num_f;
-        asteroid_grav.num_e = num_e;
-        asteroid_grav.num_v = num_v;
-
-        asteroid_grav.G = G;
-        asteroid_grav.sigma = sigma;
+            # asteroid_grav.G = G;
+            # asteroid_grav.sigma = sigma;
 
