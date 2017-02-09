35133519b1a233889f104bd2ae86a10dd8a1ce7b
Shankar Kulumani <shanks.k@gmail.com>
2017-02-09 17:31:37 -0500

    Correct density and incorporate potential funciton testing in matlab

--- test_polyhedron_shape_input.m
+++ test_polyhedron_shape_input.m
@@ -4,13 +4,15 @@ clc
 
 addpath(genpath(['.',filesep]));
 
-load('/Users/shankar/Drive/GWU/research/asteroid_dumbbell/dynamics/CASTALIA/castalia_model.mat')
+load('./dynamics/CASTALIA/castalia_model.mat')
 
 asteroid_params.F = F_32;
 asteroid_params.V = V_32;
 asteroid_params.G = 6.673e-20; % km^3/kg/sec^2
-asteroid_params.sigma = 1.9;
+asteroid_params.sigma = 2.1;
 
 asteroid_grav = polyhedron_shape_input(asteroid_params);
 
+state = [1;0.2;0];
 
+[U_mat,Ug_mat,Ug_mat_mat, Ulap_mat] = polyhedron_potential(state, asteroid_grav);
