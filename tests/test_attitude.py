a9c95f1e0c5cf6b643ff371902c5b3cd75a39e4e
Shankar Kulumani SEAS-Linux <skulumani@gwmail.gwu.edu>
2017-02-14 11:23:10 -0500

    More testing functions for attitude

--- tests/test_attitude.py
+++ tests/test_attitude.py
@@ -17,3 +17,32 @@ class TestHatAndVeeMap():
     def test_hat_map_cross_product(self):
         np.testing.assert_allclose(attitude.hat_map(self.x).dot(self.y), np.cross(self.x, self.y))
         np.testing.assert_allclose(attitude.hat_map(self.x).dot(self.y), -attitude.hat_map(self.y).dot(self.x))      
+
+
+class TestEulerRot():
+    angle = (2*np.pi- 0) * np.random.rand(1) + 0
+
+    def test_rot1_orthogonal(self):
+        mat = attitude.rot1(self.angle)
+        np.testing.assert_allclose(mat.T.dot(mat), np.eye(3,3))
+
+    def test_rot2_orthogonal(self):
+        mat = attitude.rot2(self.angle)
+        np.testing.assert_allclose(mat.T.dot(mat), np.eye(3,3))
+
+    def test_rot3_orthogonal(self):
+        mat = attitude.rot3(self.angle)
+        np.testing.assert_allclose(mat.T.dot(mat), np.eye(3,3))
+
+    def test_rot1_determinant_1(self):
+        mat = attitude.rot1(self.angle)
+        np.testing.assert_allclose(np.linalg.det(mat), 1)
+
+    def test_rot2_determinant_1(self):
+        mat = attitude.rot2(self.angle)
+        np.testing.assert_allclose(np.linalg.det(mat), 1)
+
+    def test_rot3_determinant_1(self):
+        mat = attitude.rot3(self.angle)
+        np.testing.assert_allclose(np.linalg.det(mat), 1)
+
