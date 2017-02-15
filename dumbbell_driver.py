aefe4e5122451ec615388a0496fa0920006b900d
Shankar Kulumani SEAS-Linux <skulumani@gwmail.gwu.edu>
2017-02-15 16:24:12 -0500

    Lower number of steps for testing

--- dumbbell_driver.py
+++ dumbbell_driver.py
@@ -30,8 +30,8 @@ initial_state = np.hstack((initial_pos, initial_vel, initial_R, initial_w))
 
 # time span
 t0 = 0
-tf = 1e6 # sec
-num_steps = 1e4
+tf = 1e3 # sec
+num_steps = 1e3
 
 time = np.linspace(t0,tf,num_steps)
 
