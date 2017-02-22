4bb74976b171ce6aa4183deb56c1b7074473041d
Shankar Kulumani SEAS-Linux <skulumani@gwmail.gwu.edu>
2017-02-22 11:16:25 -0500

    Compute the KE,PE and save it to a new data file

--- hpc_analysis.py
+++ hpc_analysis.py
@@ -12,12 +12,13 @@ import matplotlib.colors as colors
 
 with np.load('data/20170218_long_inertial.npz', allow_pickle=True) as data:
 
-    dum = dumbbell.Dumbbell()
-    ast = asteroid.Asteroid('castalia',1024)
-    time = np.linspace(0,1e6,1e6)
-    # dum = data['dum']
-    
+    ast_name = data['ast_name']
+    num_faces = data['num_faces']
+    time = data['time']
     state = data['state']
+
+    dum = dumbbell.Dumbbell()
+    ast = asteroid.Asteroid(data['ast_name'],data['num_faces'])
 
     KE, PE = dum.inertial_energy(time,state,ast)
 
@@ -30,3 +31,7 @@ with np.load('data/20170218_long_inertial.npz', allow_pickle=True) as data:
     plotting.plot_energy(time,KE,PE,energy_fig)
 
     plt.show()
+
+np.savez('data/energy_added.npz',ast_name=ast_name,num_faces=num_faces,time=time,state=state,KE=KE,PE=PE)
+
+
