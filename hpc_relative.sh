8cd587d2fc92d95e42b4d6c6049a42fa31df3b59
Shankar Kulumani <shanks.k@gmail.com>
2017-03-07 20:29:23 -0500

    Add a batch script for relative eoms

---
+++ hpc_relative.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# set output and error output filenames, %j will be replaced by Slurm with the jobid
+#SBATCH -o dbrel_%j.out
+#SBATCH -e dbrel_%j.err 
+
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=skulumani@gwu.edu
+
+#SBATCH -N 1
+#SBATCH -p short
+
+# set the correct directory - cloned via git
+#SBATCH -D /home/skulumani/asteroid_dumbbell
+
+#SBATCH -J dbrel
+#SBATCH --export=NONE
+
+#SBATCH -t 02-00:00:00
+
+module load anaconda/4.2.0
+
+python3 relative_driver.py castalia 256 1e6 1e6 castalia_256_1e6_relative.npz -m 0
