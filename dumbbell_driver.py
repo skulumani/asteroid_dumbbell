d5e95e8c1a31038fc71ad26ab210352681502ed2
Shankar Kulumani SEAS-Linux <skulumani@gwmail.gwu.edu>
2017-03-10 16:10:58 -0500

    Testing relative equations of motion

--- dumbbell_driver.py
+++ dumbbell_driver.py
@@ -12,7 +12,6 @@ import dynamics.dumbbell as dumbbell
 import kinematics.attitude as attitude
 import plotting
 
-
 # ode options
 RelTol = 1e-6
 AbsTol = 1e-6
@@ -22,10 +21,24 @@ dum = dumbbell.Dumbbell()
 
 # time span
 t0 = 0
-tf = 1e3 # sec
-num_steps = 1e3
+tf = 1e5 # sec
+num_steps = 1e5
 
 time = np.linspace(t0,tf,num_steps)
+
+periodic_pos = np.array([1.495746722510590,0.000001002669660,0.006129720493607]) 
+periodic_vel = np.array([0.000000302161724,-0.000899607989820,-0.000000013286327])
+
+state = integrate.odeint(dum.eoms_relative_translation, np.hstack((periodic_pos,periodic_vel)), time, args=(ast,), atol=AbsTol, rtol=RelTol)
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+ax.plot(state[:,0], state[:,1], state[:,2])
+ax.set_xlim([-3, 3])
+ax.set_ylim([-3, 3])
+ax.set_zlim([-3, 3])
+plt.show()
 
 def inertial_test():
     # set initial state
@@ -84,7 +97,7 @@ def relative_test():
     R = state[:,6:15]
     ang_vel = state[:,15:18]
 
-    KE, PE = dum.relative_energy(time,state,ast)
+    # KE, PE = dum.relative_energy(time,state,ast)
 
     mpl.rcParams['legend.fontsize'] = 10
 
@@ -93,7 +106,38 @@ def relative_test():
     plotting.plot_trajectory(pos,traj_fig)
 
     # kinetic energy
-    energy_fig = plt.figure()
-    plotting.plot_energy(time,KE,PE,energy_fig)
+    # energy_fig = plt.figure()
+    # plotting.plot_energy(time,KE,PE,energy_fig)
 
     plt.show()
+
+    return state
+
+
+def eoms_relative_translation(state, t, ast, dum):
+        """Translational equations of motion
+
+        Inputs:
+            state - 
+                pos - 3x1 position vector in asteroid fixed frame
+                vel - 3x1 velocity vector in asteroid fixed frame
+
+        Outputs:
+            statedot
+
+        """
+        # unpack the state
+        pos = state[0:3]
+        vel = state[3:6]
+        # unpack constants
+        omega_hat = attitude.hat_map(np.array([0, 0, ast.omega]))
+        (_, U_grad, _, _) = ast.polyhedron_potential(pos)
+
+        m = dum.m1 + dum.m2
+        # state derivatives
+        pos_dot = vel
+        vel_dot = U_grad - 2 * omega_hat.dot(vel) - omega_hat.dot(omega_hat.dot(pos))
+
+        state_dot = np.hstack((pos_dot, vel_dot))
+        
+        return state_dot
