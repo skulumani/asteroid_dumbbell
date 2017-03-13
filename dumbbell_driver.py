1ecaafccf95c19b0852d8f4dc4ffa33443d0b3de
Shankar Kulumani <shanks.k@gmail.com>
2017-03-13 13:51:11 -0400

    Fix initial condition for relative equations of motion

--- dumbbell_driver.py
+++ dumbbell_driver.py
@@ -12,34 +12,6 @@ import dynamics.dumbbell as dumbbell
 import kinematics.attitude as attitude
 import plotting
 
-# ode options
-RelTol = 1e-6
-AbsTol = 1e-6
-
-ast = asteroid.Asteroid('castalia',32)
-dum = dumbbell.Dumbbell()
-
-# time span
-t0 = 0
-tf = 1e5 # sec
-num_steps = 1e5
-
-time = np.linspace(t0,tf,num_steps)
-
-periodic_pos = np.array([1.495746722510590,0.000001002669660,0.006129720493607]) 
-periodic_vel = np.array([0.000000302161724,-0.000899607989820,-0.000000013286327])
-
-state = integrate.odeint(dum.eoms_relative_translation, np.hstack((periodic_pos,periodic_vel)), time, args=(ast,), atol=AbsTol, rtol=RelTol)
-
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-
-ax.plot(state[:,0], state[:,1], state[:,2])
-ax.set_xlim([-3, 3])
-ax.set_ylim([-3, 3])
-ax.set_zlim([-3, 3])
-plt.show()
-
 def inertial_test():
     # set initial state
     initial_pos = np.array([1.495746722510590,0.000001002669660,0.006129720493607]) # km for center of mass in body frame
@@ -113,7 +85,6 @@ def relative_test():
 
     return state
 
-
 def eoms_relative_translation(state, t, ast, dum):
     """Translational equations of motion
 
@@ -150,46 +121,61 @@ def eoms_relative(state, t, ast, dum):
         Ra = attitude.rot3(ast.omega*t, 'c') # asteroid body frame to inertial frame
 
         # unpack parameters for the dumbbell
-        m1 = self.m1
-        m2 = self.m2
+        m1 = dum.m1
+        m2 = dum.m2
         m = m1 + m2
-        J = self.J
-        Jr = R.dot(J).dot(R.T)
+        J = dum.J
+
         Omega = ast.omega*np.array([0,0,1]) # angular velocity vector of asteroid
 
         # the position of each mass in the asteroid body frame
-        rho1 = self.zeta1
-        rho2 = self.zeta2
-
-        # position of each mass in the asteroid frame
-        z1 = pos + R.dot(rho1)
-        z2 = pos + R.dot(rho2)
+        rho1 = dum.zeta1
+        rho2 = dum.zeta2
 
         z = pos # position of COM in asteroid frame
 
         # compute the potential at this state
-        (U1, U1_grad, U1_grad_mat, U1laplace) = ast.polyhedron_potential(z1)
-        (U2, U2_grad, U2_grad_mat, U2laplace) = ast.polyhedron_potential(z2)
+        (U, U_grad, U_grad_mat, Ulaplace) = ast.polyhedron_potential(z)
 
         # force due to each mass expressed in asteroid body frame
-        F1 = m1*U1_grad
-        F2 = m2*U2_grad
-        # F_com = (m1+m2)*U_grad_com
-
-        # compute the moments due to each mass
-        M1 = m1 * np.cross(U1_grad, R.dot(rho1))
-        M2 = m2 * np.cross(U2_grad, R.dot(rho2))
+        F_com = m*U_grad
 
         # state derivatives
         pos_dot = vel - attitude.hat_map(Omega).dot(pos)
-        vel_dot = 1/m * (F1 + F2 - m * attitude.hat_map(Omega).dot(vel))
-        # vel_dot = 1/m * (F_com) 
-        R_dot = attitude.hat_map(w).dot(R) - attitude.hat_map(Omega).dot(R)
-        R_dot = R_dot.reshape(9)
-        w_dot = np.linalg.inv(Jr).dot(M1 + M2 - attitude.hat_map(Omega).dot(Jr).dot(w) 
-                      + attitude.hat_map(w).dot(Jr).dot(w) - Jr.dot(attitude.hat_map(Omega)).dot(w) 
-                      + attitude.hat_map(Omega).dot(Jr).dot(w) - attitude.hat_map(w).dot(Jr).dot(w))
+        vel_dot = 1/m * (F_com - m * attitude.hat_map(Omega).dot(vel))
 
-        state_dot = np.hstack((pos_dot,vel_dot,R_dot,w_dot))
+        state_dot = np.hstack((pos_dot,vel_dot))
         
         return state_dot
+
+
+if __name__ == '__main__':
+    # ode options
+    RelTol = 1e-6
+    AbsTol = 1e-6
+
+    ast = asteroid.Asteroid('castalia',32)
+    dum = dumbbell.Dumbbell()
+
+    # time span
+    t0 = 0
+    tf = 1e5 # sec
+    num_steps = 1e5
+
+    time = np.linspace(t0,tf,num_steps)
+
+    periodic_pos = np.array([1.495746722510590,0.000001002669660,0.006129720493607]) 
+    periodic_vel = np.array([0.000000302161724,-0.000899607989820,-0.000000013286327]) 
+
+    # state = integrate.odeint(eoms_relative_translation, np.hstack((periodic_pos,periodic_vel)), time, args=(ast,dum,), atol=AbsTol, rtol=RelTol)
+
+    state = integrate.odeint(eoms_relative, np.hstack((periodic_pos,periodic_vel + attitude.hat_map(ast.omega*np.array([0,0,1])).dot(periodic_pos))), time, args=(ast,dum,), atol=AbsTol, rtol=RelTol)
+
+    fig = plt.figure()
+    ax = fig.gca(projection='3d') 
+
+    ax.plot(state[:,0], state[:,1], state[:,2])
+    ax.set_xlim([-3, 3])
+    ax.set_ylim([-3, 3])
+    ax.set_zlim([-3, 3])
+    plt.show()
