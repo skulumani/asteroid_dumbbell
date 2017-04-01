Fix moment computation

dumbbell_driver.py
@@ -34,7 +34,7 @@ initial_pos = periodic_pos # km for center of mass in body frame
 # km/sec for COM in asteroid fixed frame
 initial_vel = periodic_vel + attitude.hat_map(ast.omega*np.array([0,0,1])).dot(initial_pos)
 initial_R = attitude.rot3(np.pi/2, 'c').reshape(9) # transforms from dumbbell body frame to the inertial frame
-initial_w = np.array([0.001,0.001,0.001]) # angular velocity of dumbbell wrt to inertial frame represented in sc body frame
+initial_w = np.array([0.01,0.01,0.01]) # angular velocity of dumbbell wrt to inertial frame represented in sc body frame
 
 initial_state = np.hstack((initial_pos, initial_vel, initial_R, initial_w))
 
@@ -56,7 +56,7 @@ def inertial_test(filename=''):
     plotting.plot_trajectory(pos, traj_fig)
 
     # animation testing
-    plotting.animate_inertial_trajectory(time, state, ast, dum, filename)
+    # plotting.animate_inertial_trajectory(time, state, ast, dum, filename)
 
     # energy plot
     KE, PE = dum.inertial_energy(time, state, ast)

dynamics/dumbbell.py
@@ -67,11 +67,14 @@ class Dumbbell(object):
         F2 = self.m2*Ra.dot(U2_grad)
 
         # pdb.set_trace()
-        M1 = self.m1 * np.cross(Ra.T.dot(rho1),R.T.dot(U1_grad))
-        M2 = self.m2 * np.cross(Ra.T.dot(rho2),R.T.dot(U2_grad))
+        # M1 = self.m1 * np.cross(Ra.T.dot(rho1),R.T.dot(U1_grad))
+        # M2 = self.m2 * np.cross(Ra.T.dot(rho2),R.T.dot(U2_grad))
         # M1 = np.zeros(3)
         # M2 = M1
         
+        M1 = self.m1 * np.cross(rho1, R.T.dot(Ra).dot(U1_grad))
+        M2 = self.m2 * np.cross(rho2, R.T.dot(Ra).dot(U2_grad))
+
         pos_dot = vel
         vel_dot = 1/(self.m1+self.m2) *(F1 + F2)
         R_dot = R.dot(attitude.hat_map(ang_vel)).reshape(9)

eom_comparison.py
@@ -85,9 +85,9 @@ def relative2inertial(time, state, ast, dum):
 
 def load_data(inertial_filename, relative_filename, mode):
     # load simulation data
-    inertial_filename = 'data/inertial_energy_castalia_64_1e5_inertial.npz'
-    relative_filename = 'data/relative_energy_castalia_64_1e5_relative.npz'
-    mode = 0
+    # inertial_filename = 'inertial_energy_test.npz'
+    # relative_filename = 'relative_energy_test.npz'
+    # mode = 0
     with np.load(inertial_filename, allow_pickle=True) as data:
         inertial_state = data['state']
         inertial_time = data['time']