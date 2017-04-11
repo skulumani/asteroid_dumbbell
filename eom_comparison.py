8afc2e09e73927186bb296d78d5e884247d23569
Shankar Kulumani <shanks.k@gmail.com>
2017-04-11 17:21:59 -0400

    Another function to transform the state
    Transforms inertial eoms to the asteroid fixed frame to allow for
    comparision with the relative eoms.

--- eom_comparison.py
+++ eom_comparison.py
@@ -184,7 +184,64 @@ def body2inertial(time, state, ast, dum):
 
     return inertial_state 
 
+def body2ast(time, state, ast, dum):
+    """Convert state from the body frame to the asteroid fixed frame
 
+    This function will convert the state output of eoms_inertial to the asteroid fixed frame.
+    This wil allow for comparision with the state from eoms_relative.
+
+    Inputs:
+        time - nx1 array of time stamps for the state vector
+        state - nx18 array of the state vector defined as follows:
+            inertial_pos - state[:, 0:3] is the position of the body with respect to the asteroid 
+                center and defined in the inertial frame
+            inertial_vel - state[:, 4:6] is the velocity of the sc with respect to the asteroid 
+                center of mass and defined in the inertial frame
+            R_sc2int - state[:, 6:15] the rotation matrix which transforms vectors from the sc frame
+                to the inertial frame
+            body_w - state[:, 15:18] the angular velocity of the sc with respect to the inertial
+                frame and defined in the spacecraft frame
+        ast - Instance of Asteroid class
+        dum - Instance of Dumbbell class
+
+    Outputs:
+        ast_state - the state as represented in the rotating asteroid frame
+            ast_pos - ast_state[:, 0:3] is the position of the sc with respect to the asteroid
+                fixed frame and represented in the asteroid frame
+            ast_vel - ast_state[:, 3:6] is the velocity of the sc with respect to the asteroid and 
+                represented in the asteroid fixed frame
+            ast_R_sc2ast - ast_state[:, 6:15] is the transformation from the sc frame to the asteroid
+                frame
+            ast_w - ast_state[:, 15:18] is the angular velocity of the sc with respect to the inertial 
+                frame and defined in the asteroid frame
+    """
+    # transformation between asteroid fixed frame and inertial frame
+    # figure out transformation from inertial frame to relative frame
+    Rast2int = np.zeros((3, 3, time.shape[0]))
+    Rint2ast = np.zeros((3, 3, time.shape[0]))
+
+    ast_state = np.zeros(state.shape)
+
+    for ii, t in np.ndenumerate(time):
+        Rast2int[:, :, ii] = attitude.rot3(ast.omega*t, 'c')[:, :, np.newaxis] # asteroid body frame to inertial frame
+        Rint2ast[:, :, ii] = attitude.rot3(ast.omega*t, 'c').T[:, :, np.newaxis]
+
+        Ra = np.squeeze(Rast2int[:, :, ii])
+        # convert the relative state to the inertial frame
+        body_pos = np.squeeze(state[ii, 0:3])
+        body_vel = np.squeeze(state[ii, 3:6])
+        R_sc2int = np.squeeze(state[ii, 6:15].reshape(3, 3))
+        body_w = np.squeeze(state[ii, 15:18])
+
+        ast_pos = Ra.T.dot(body_pos)
+        ast_vel = Ra.T.dot(body_vel)
+        ast_R_sc2ast = Ra.T.dot(R_sc2int).reshape(9)
+        ast_w = ast_R_sc2ast.reshape((3, 3)).dot(body_w)
+
+        ast_state[ii, :] = np.hstack((ast_pos, ast_vel, ast_R_sc2ast, ast_w))
+
+    return ast_states
+    
 def load_data(inertial_filename, relative_filename, mode):
     # load simulation data
     # inertial_filename = 'inertial_energy_test.npz'
