5c9094490160fa4af932916f136b5c1b466ca918
Shankar Kulumani <shanks.k@gmail.com>
2017-04-13 17:24:47 -0400

    Add some print statements to the eom comparison

--- eom_comparison/utilities.py
+++ eom_comparison/utilities.py
@@ -56,20 +56,27 @@ def inertial_frame_comparison(ast_name='castalia', num_faces=64, tf=1e5, num_ste
 
     """
     initial_w = np.array([0.01, 0.0, 0.0])
-
+    
+    print("Running inertial EOMS")
     i_time, i_state = id.inertial_eoms_driver(ast_name, num_faces, tf, num_steps, initial_w)
+
+    print("Running asteroid EOMs")
     r_time, r_state = rd.relative_eoms_driver(ast_name, num_faces, tf, num_steps, initial_w)
 
     ast = asteroid.Asteroid(ast_name,num_faces)
     dum = dumbbell.Dumbbell()
 
     # also compute and compare the energy behavior
+    print("Computing inertial energy")
     i_KE, i_PE = dum.inertial_energy(i_time, i_state, ast)
+
+    print("Computing asteroid energy")
     r_KE, r_PE = dum.relative_energy(r_time, r_state, ast)
 
     plotting.plot_energy(i_time, i_KE, i_PE)
     # also look at the animation of both and the converted form as well
     
+    print("Plot comparison in the inertial frame")
     plotting.plot_inertial_comparison(r_time, i_time, r_state, i_state, ast, dum) 
 
     return 0
