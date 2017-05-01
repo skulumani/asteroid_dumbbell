5d6f5cf46bf5e3711798bbba186fd18d54d7beea
Shankar Kulumani <shanks.k@gmail.com>
2017-05-01 15:00:26 -0400

    Try tumbling motion

--- eom_compare.py
+++ eom_compare.py
@@ -16,8 +16,8 @@ RelTol = 1e-9
 AbsTol = 1e-9
 ast_name = 'castalia'
 num_faces = 64
-tf = 1e3
-num_steps = 1e3
+tf = 1e4
+num_steps = 1e5
 time = np.linspace(0,tf,num_steps)
 
 periodic_pos = np.array([1.495746722510590,0.000001002669660,0.006129720493607])
@@ -31,7 +31,7 @@ print("Running inertial EOMS")
 initial_pos = periodic_pos # km for center of mass in body frame
 initial_vel = periodic_vel + attitude.hat_map(ast.omega*np.array([0,0,1])).dot(initial_pos)
 initial_R = attitude.rot2(np.pi/2).reshape(9) # transforms from dumbbell body frame to the inertial frame
-initial_w = np.array([0.00, 0.01, 0.00])
+initial_w = np.array([0.01, 0.01, 0.01])
 initial_state = np.hstack((initial_pos, initial_vel, initial_R, initial_w))
 
 i_state = integrate.odeint(dum.eoms_inertial, initial_state, time, args=(ast,), atol=AbsTol, rtol=RelTol)
@@ -59,13 +59,13 @@ for ii in range(rh_state.shape[0]):
 rh_state_conv = np.hstack((rh_state[:, 0:3], rh_vel, rh_state[:, 6:15], rh_ang_vel))
 
 # also compute and compare the energy behavior
-print("Computing inertial energy")
-i_KE, i_PE = dum.inertial_energy(time, i_state, ast)
+# print("Computing inertial energy")
+# i_KE, i_PE = dum.inertial_energy(time, i_state, ast)
 
-print("Computing asteroid energy")
-r_KE, r_PE = dum.relative_energy(time, rh_state_conv, ast)
+# print("Computing asteroid energy")
+# r_KE, r_PE = dum.relative_energy(time, rh_state_conv, ast)
  
-plotting.plot_energy(time, i_KE, i_PE)
+# plotting.plot_energy(time, i_KE, i_PE)
 
 print("Plot comparison in the inertial frame")
-plotting.plot_inertial_comparison(time,time, rh_state_conv, i_state, ast, dum)
+plotting.plot_inertial_comparison(time,time, rh_state_conv, i_state, ast, dum, False, 1)
