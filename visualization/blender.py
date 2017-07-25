e39c2b7684c34afc1628ee0845e2f11bcf765409
Shankar Kulumani <shanks.k@gmail.com>
2017-07-24 20:38:56 -0400

    Start on splitting up the blender functions

--- visualization/blender.py
+++ visualization/blender.py
@@ -10,64 +10,159 @@ import os
 import bpy
 import numpy as np
 
-def look_at(obj_camera, point):
-    loc_camera = obj_camera.matrix_world.to_translation()
+def look_at(camera, point):
+    r"""Ensure camera is pointing at object
+
+    This function will ensure that within a blender scene that the camera is always pointed at the object
+
+    Parameters
+    ----------
+    camera : blender object
+        camera object
+    point : blender point object
+        the object to look at
+
+    Returns
+    -------
+    None
+
+    Other Parameters
+    ----------------
+    None
+
+    Raises
+    ------
+    None
+
+    See Also
+    --------
+
+    Notes
+    -----
+
+    Author
+    ------
+    Shankar Kulumani		GWU		skulumani@gwu.edu
+
+    References
+    ----------
+
+    """ 
+    
+    loc_camera = camera.matrix_world.to_translation()
 
     direction = point - loc_camera
 
     rot_quat = direction.to_track_quat('-Z', 'Y')
-    obj_camera.rotation_euler = rot_quat.to_euler()
+    camera.rotation_euler = rot_quat.to_euler()
 
-scene = bpy.context.scene
+def load_asteroid(asteroid):
+    """Load the desired asteroid
 
-output_path = 'visualization/blender'
+    doc
+    """
 
-# set scene render options
-scene.render.resolution_percentage = 100
-# scene.render.filepath = 'visualization/blender/'
-scene.render.engine = 'BLENDER_RENDER'
+    # import OBJ model
+    bpy.ops.import_scene.obj(filepath='data/itokawa_low.obj')
 
-# import OBJ model
-bpy.ops.import_scene.obj(filepath='data/itokawa_low.obj')
+    camera = bpy.data.objects['Camera']
+    lamp = bpy.data.objects['Lamp']
+    itokawa = bpy.data.objects['itokawa_low']
+    layers = [False] * 32
+    layers[0] = True
 
-# add a ground plane
-add_plane = bpy.ops.mesh.primitive_plane_add
-add_cube = bpy.ops.mesh.primitive_cube_add
-cube = bpy.data.objects['Cube']
-camera = bpy.data.objects['Camera']
-lamp = bpy.data.objects['Lamp']
-itokawa = bpy.data.objects['itokawa_low']
-layers = [False] * 32
-layers[0] = True
+    bpy.types.UnitSettings.system = 'METRIC'
+    bpy.types.UnitSettings.scale_length = 1e3
 
-bpy.types.UnitSettings.system = 'METRIC'
-bpy.types.UnitSettings.scale_length = 1e3
+    # delete the cube
+    radians = np.arange(0, 2*np.pi, 1*np.pi/180)
 
-# delete the cube
-radians = np.arange(0, 2*np.pi, 1*np.pi/180)
+    itokawa.location.z = 2
 
-itokawa.location.z = 2
+    lamp.location.x = 0
+    lamp.location.y = 0 
+    lamp.location.z = 5
+    for ii,rad in enumerate(radians):
+        cube.location.x = np.sin(rad)
+        itokawa.rotation_euler.z = rad * 180 / np.pi
+        camera.location.x = np.sin(rad)
+        look_at(camera, itokawa.matrix_world.to_translation())
+        scene.render.filepath = os.path.join(output_path, 'cube_' + str(ii)  + '.png')
+        bpy.ops.render.render(write_still=True)
 
-# camera.location.y = -2
-# camera.location.z = 2
-# camera.rotation_euler.x = -90
-# camera.rotation_euler.y = 0
-# camera.rotation_euler.z = 0
+    for object in bpy.data.objects:
+        print(object.name + " is at location " + str(object.location))
 
-lamp.location.x = 0
-lamp.location.y = 0 
-lamp.location.z = 5
-for ii,rad in enumerate(radians):
-    cube.location.x = np.sin(rad)
-    itokawa.rotation_euler.z = rad * 180 / np.pi
-    camera.location.x = np.sin(rad)
-    look_at(camera, itokawa.matrix_world.to_translation())
-    scene.render.filepath = os.path.join(output_path, 'cube_' + str(ii)  + '.png')
-    bpy.ops.render.render(write_still=True)
+def reset_blend():
+    """Reset blender
+    """
+    bpy.ops.wm.read_factory_settings()
 
-for object in bpy.data.objects:
-    print(object.name + " is at location " + str(object.location))
+    for scene in bpy.data.scenes:
+        for obj in scene.objects:
+            scene.objects.unlink(obj)
+
+    # only worry about data in the startup scene
+    for bpy_data_iter in (
+            bpy.data.objects,
+            bpy.data.meshes,
+            bpy.data.lamps,
+            bpy.data.cameras,
+            ):
+        for id_data in bpy_data_iter:
+            bpy_data_iter.remove(id_data)
+
+def blender_example():
+    scene = bpy.context.scene
+
+    output_path = 'visualization/blender'
+
+    # set scene render options
+    scene.render.resolution_percentage = 100
+    # scene.render.filepath = 'visualization/blender/'
+    scene.render.engine = 'BLENDER_RENDER'
+
+    # import OBJ model
+    bpy.ops.import_scene.obj(filepath='data/itokawa_low.obj')
+
+    # add a ground plane
+    add_plane = bpy.ops.mesh.primitive_plane_add
+    add_cube = bpy.ops.mesh.primitive_cube_add
+    cube = bpy.data.objects['Cube']
+    camera = bpy.data.objects['Camera']
+    lamp = bpy.data.objects['Lamp']
+    itokawa = bpy.data.objects['itokawa_low']
+    layers = [False] * 32
+    layers[0] = True
+
+    bpy.types.UnitSettings.system = 'METRIC'
+    bpy.types.UnitSettings.scale_length = 1e3
+
+    # delete the cube
+    radians = np.arange(0, 2*np.pi, 1*np.pi/180)
+
+    itokawa.location.z = 2
+
+    # camera.location.y = -2
+    # camera.location.z = 2
+    # camera.rotation_euler.x = -90
+    # camera.rotation_euler.y = 0
+    # camera.rotation_euler.z = 0
+
+    lamp.location.x = 0
+    lamp.location.y = 0 
+    lamp.location.z = 5
+    for ii,rad in enumerate(radians):
+        cube.location.x = np.sin(rad)
+        itokawa.rotation_euler.z = rad * 180 / np.pi
+        camera.location.x = np.sin(rad)
+        look_at(camera, itokawa.matrix_world.to_translation())
+        scene.render.filepath = os.path.join(output_path, 'cube_' + str(ii)  + '.png')
+        bpy.ops.render.render(write_still=True)
+
+    for object in bpy.data.objects:
+        print(object.name + " is at location " + str(object.location))
 
 
-# bpy.ops.import_scene.obj('../data/itokawa_low.obj')
-# bpy.ops.render.render(write_still=True)
+    # bpy.ops.import_scene.obj('../data/itokawa_low.obj')
+    # bpy.ops.render.render(write_still=True)
