ce4bc2de956e96e10247caadd472596b93a2fe1b
Shankar Kulumani <shanks.k@gmail.com>
2017-08-02 14:53:33 -0400

    Have some more feature matching code

--- visualization/opencv.py
+++ visualization/opencv.py
@@ -278,4 +278,131 @@ def sift_ratio_test_matching(filename1, filename2, plot=False):
 
     return good 
 
+def sift_flann_matching(filename1, filename2, plot=False): 
+    img1 = cv2.imread(filename1)
+    img2 = cv2.imread(filename2)
 
+    kp1, des1, _ = sift(filename1)
+    kp2, des2, _ = sift(filename2)
+
+    # FLANN parameters
+    FLANN_INDEX_KDTREE = 0
+    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    search_params = dict(checks=50)  # or empty dictionary
+
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    
+    matches = flann.knnMatch(des1, des2, k=2)
+
+    # draw only good matches by creating a mask
+    matchesMask = [[0, 0] for i in range(len(matches))]
+
+    # ratio test 
+    for i, (m, n) in enumerate(matches):
+        if m.distance < 0.7 * n.distance:
+            matchesMask[i] = [1, 0]
+
+    if plot:
+        draw_params = dict(matchColor = (0, 255, 0),
+                        singlePointColor = (255, 0, 0),
+                        matchesMask = matchesMask,
+                        flags = 0)
+
+        img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, matches, None, **draw_params)
+
+        plt.imshow(img3)
+        plt.show()
+
+    return matches
+    
+def orb_flann_matching(filename1, filename2, plot=False): 
+    img1 = cv2.imread(filename1)
+    img2 = cv2.imread(filename2)
+
+    kp1, des1, _ = orb(filename1)
+    kp2, des2, _ = orb(filename2)
+
+    # FLANN parameters
+    FLANN_INDEX_LSH = 6
+    index_params = dict(algorithm = FLANN_INDEX_LSH,
+                        table_number = 6,  # 12
+                        key_size = 12,  # 20
+                        multi_probe_level = 1)  # 2
+    search_params = dict(checks=50)  # or empty dictionary
+
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    
+    matches = flann.knnMatch(des1, des2, k=2)
+
+    # draw only good matches by creating a mask
+    matchesMask = [[0, 0] for i in range(len(matches))]
+
+    # ratio test 
+    for i, (m, n) in enumerate(matches):
+        if m.distance < 0.7 * n.distance:
+            matchesMask[i] = [1, 0]
+
+    if plot:
+        draw_params = dict(matchColor = (0, 255, 0),
+                        singlePointColor = (255, 0, 0),
+                        matchesMask = matchesMask,
+                        flags = 0)
+
+        img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, matches, None, **draw_params)
+
+        plt.imshow(img3)
+        plt.show()
+
+    return matches
+
+def sift_feature_homography(filename1, filename2):
+    MIN_MATCH_COUNT = 10
+
+    img1 = cv2.imread(filename1)
+    img2 = cv2.imread(filename2)
+
+    # sift keypoints
+    kp1, des1, _ = sift(filename1)
+    kp2, des2, _ = sift(filename2)
+
+    FLANN_INDEX_KDTREE = 0
+    index_params = dict(algorithm = FLANN_INDEX_KDTREE, tree = 5)
+    search_params = dict(checks = 50)
+
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+    matches = flann.knnMatch(des1, des2, k=2)
+
+    # store the good matches using a ratio test
+    good = []
+    for m,n in matches:
+        if m.distance < 0.7 * n.distance:
+            good.append(m)
+
+    if len(good) > MIN_MATCH_COUNT:
+        src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1, 1, 2)
+        dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1, 1, 2)
+
+        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+        matchesMask = mask.ravel().tolist()
+
+        h, w, d = img1.shape
+        pts = np.float32([ [0,0], [0, h-1], [w-1, h-1], [w-1, 0] ]).reshape(-1, 1, 2)
+        dst = cv2.perspectiveTransform(pts, M)
+
+        img2 = cv2.polylines(img2, [np.int32(dst)], True, 2255, 3, cv2.LINE_AA)
+
+    else:
+        print("Not enough matches are found - {0}/{1}".format((len(good), MIN_MATCH_COUNT)))
+        matchesMask = None
+
+    draw_params = dict(matchColor = (0, 255, 0), # draw matches in green
+                       singlePointColor = None, 
+                       matchesMask = matchesMask, # draw only the inliers
+                       flags = 2)
+
+    img3 = cv2.drawMatches(img1, kp1, img2, kp2, good, None, **draw_params)
+    plt.imshow(img3, 'gray')
+    plt.show()
+
+    print(M) 
