7c28b10828fac297072efa0c4f39c7f55a3d607f
Shankar Kulumani <shanks.k@gmail.com>
2017-08-04 15:16:51 -0400

    Add a test function for opencv

---
+++ tests/test_opencv.py
@@ -0,0 +1,21 @@
+"""Ensure that blender module is compiled and working properly
+
+"""
+import sys
+import numpy as np
+import cv2
+
+def test_python_version():
+    req_version = (3, 5, 3, 'final', 0)
+    cur_version = sys.version_info
+    np.testing.assert_equal(cur_version, req_version)
+
+def test_anaconda_enviornment():
+    asteroid_env = '/home/shankar/anaconda3/envs/asteroid'
+    np.testing.assert_equal(sys.prefix, asteroid_env)
+
+def test_opencv_version():
+    req_version = '3.2.0'
+    cur_version = cv2.__version__
+    np.testing.assert_equal(cur_version, req_version)
+
