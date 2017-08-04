b5e022f531fb7b8eb77a54159c799c6231da908f
Shankar Kulumani <shanks.k@gmail.com>
2017-08-04 13:26:00 -0400

    Test for blender version

--- tests/test_blender.py
+++ tests/test_blender.py
@@ -4,6 +4,7 @@
 import sys
 import numpy as np
 
+import bpy
 def test_python_version():
     req_version = (3, 5, 3, 'final', 0)
     cur_version = sys.version_info
@@ -13,7 +14,11 @@ def test_anaconda_enviornment():
     asteroid_env = '/home/shankar/anaconda3/envs/asteroid'
     np.testing.assert_equal(sys.prefix, asteroid_env)
 
+def test_blender_version():
+    req_version = (2, 79, 0)
+    cur_version = bpy.app.version
+    np.testing.assert_equal(cur_version, req_version)
+
 def test_blender_installed():
-    import bpy
     result = bpy.ops.render.render(write_still=True)
     np.testing.assert_equal(result.pop(), 'FINISHED')
