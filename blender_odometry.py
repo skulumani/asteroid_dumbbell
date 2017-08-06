a388bf6fc04be27929a98db97bbeb69084769b5e
Shankar Kulumani <shanks.k@gmail.com>
2017-08-06 12:14:54 -0400

    Start on defining camera model

--- blender_odometry.py
+++ blender_odometry.py
@@ -10,15 +10,24 @@ import matplotlib.pyplot as plt
 
 from visual_odometry import PinholeCamera, VisualOdometry
 
+from visualization import plotting, blender_camera, blender
+
 # load imagery and state/time during those pictures
 sim_data = h5py.File('./data/itokawa_landing/blender_low.hdf5', 'r')
 
-K = sim_data['K']
-i_state = sim_data['i_state']
-time = sim_data['time']
+_, camera, _, _, _, _ = blender.blender_init() 
+K = blender_camera.get_calibration_matrix_K_from_blender(camera)
+K = np.array(K)
+
+# K = sim_data['K']
+# i_state = sim_data['i_state']
+# time = sim_data['time']
 images = sim_data['landing']
-RT_vector = sim_data['RT']
-R_i2bcam_vector = sim_data['R_i2bcam']
+# RT_vector = sim_data['RT']
+# R_i2bcam_vector = sim_data['R_i2bcam']
 
 # define pinhole camera model
-
+# pixel height of NEAR MSI camera
+width = 537
+height = 244 
+cam = PinholeCamera(width, height, K[0, 0], K[1, 1], K[0, 2], K[1, 2]) 
