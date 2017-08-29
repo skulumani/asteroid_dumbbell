e871ed81d87c500e2f0f8f91747ebb3cd1a9de50
Shankar Kulumani <shanks.k@gmail.com>
2017-08-29 16:51:20 -0400

    Add a test for new eoms function

--- tests/test_dumbell.py
+++ tests/test_dumbell.py
@@ -70,6 +70,37 @@ class TestDumbbellRelative():
     # def test_moment_of_inertia(self):
     #     np.testing.assert_allclose(self.dum.J, np.trace(self.dum.Jd)*np.eye(3,3) - self.dum.Jd)
 
+class TestDumbbellRelativeODE():
+    dum = dumbbell.Dumbbell()
+    angle = (2*np.pi- 0) * np.random.rand(1) + 0
+    
+    # generate a random initial state that is outside of the asteroid
+    pos = np.random.rand(3)+np.array([2,2,2])
+    vel = np.random.rand(3)
+    R = attitude.rot1(angle).reshape(9)
+    ang_vel = np.random.rand(3)
+    
+    t = np.random.rand()*100
+
+    state = np.hstack((pos,vel, R, ang_vel))
+    statedot = dum.eoms_relative_ode(t, state, ast)
+
+    def test_relative_moment_of_inertia(self):
+        R = self.R.reshape((3,3))
+        Jr = R.dot(self.dum.J).dot(R.T)
+        Jdr = R.dot(self.dum.Jd).dot(R.T)
+        np.testing.assert_array_almost_equal(
+            attitude.hat_map(Jr.dot(self.ang_vel)), 
+            attitude.hat_map(self.ang_vel).dot(Jdr) + Jdr.dot(attitude.hat_map(self.ang_vel)))
+
+    def test_eoms_relative_size(self):
+        np.testing.assert_allclose(self.statedot.shape, (18,))
+
+    def test_eoms_relative_R_dot(self):
+        np.testing.assert_allclose(self.statedot[6:15].shape, (9,))
+        
+    # def test_moment_of_inertia(self):
+    #     np.testing.assert_allclose(self.dum.J, np.trace(self.dum.Jd)*np.eye(3,3) - self.dum.Jd)
 class TestDumbbellInertialDesiredAttitude():
     
     dum = dumbbell.Dumbbell()
