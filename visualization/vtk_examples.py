d83c3f7e5bf380ce57b8ff167062b5f5c7e00614
Shankar Kulumani <shanks.k@gmail.com>
2017-10-13 17:21:19 -0400

    Add another example

--- visualization/vtk_examples.py
+++ visualization/vtk_examples.py
@@ -346,6 +346,46 @@ def step2_observer():
         time.sleep(0.03)
         renWin.Render()
         ren1.GetActiveCamera().Azimuth(1)
+
+def step3_multiple_renderer():
+    """Create multiple renderers inside a render window
+    """
+
+    cone = vtk.vtkConeSource()
+    cone.SetHeight(3.0)
+    cone.SetRadius(1.0)
+    cone.SetResolution(100)
+    
+    coneMapper = vtk.vtkPolyDataMapper()
+    coneMapper.SetInputConnection(cone.GetOutputPort())
+
+    coneActor = vtk.vtkActor()
+    coneActor.SetMapper(coneMapper)
+
+    ren1 = vtk.vtkRenderer()
+    ren1.AddActor(coneActor)
+    ren1.SetBackground(0.1, 0.2, 0.4)
+    ren1.SetViewport(0.0, 0.0, 0.5, 1.0)
+
+    ren2 = vtk.vtkRenderer()
+    ren2.AddActor(coneActor)
+    ren2.SetBackground(0.1, 0.2, 0.4)
+    ren2.SetViewport(0.5, 0.0, 1.0, 1.0)
+
+    renWin = vtk.vtkRenderWindow()
+    renWin.AddRenderer(ren1)
+    renWin.AddRenderer(ren2)
+    renWin.SetSize(600, 300)
+
+    ren1.ResetCamera()
+    ren1.GetActiveCamera().Azimuth(90)
+
+    for i in range(0, 360):
+        time.sleep(0.03)
+        renWin.Render()
+        ren1.GetActiveCamera().Azimuth(1)
+        ren2.GetActiveCamera().Azimuth(1)
+
 if __name__ == '__main__':
     vtk_cylinder()
     vtk_distance()
