3585d3e9e7a673a6f110667dca3a6428c4cc513a
Shankar Kulumani <shanks.k@gmail.com>
2017-10-19 17:27:03 -0400

    Start on wavefront reader/writer module

---
+++ point_cloud/wavefront.py
@@ -0,0 +1,33 @@
+"""Wavefront OBJ module
+
+Handle reading and writing to OBJ files. This will convert a wavefront file
+to/from a numpy array (point cloud).
+
+Author
+------
+Shankar Kulumani		GWU		skulumani@gwu.edu
+"""
+import numpy as np
+import pdb
+
+def write_obj():
+    pass
+
+def read_obj(filename):
+    """Read the OBJ file and save to a numpy array
+    """
+    faces = []
+    verts = []
+    pdb.set_trace()    
+    with open(filename, 'r') as f:
+        for line in f:
+            prefix, value = line.split(' ', 1)
+
+            if prefix == 'v':
+                verts.append([float(val) for val in value.split()]) 
+            elif prefix == 'f':
+                faces.append([int(val) for val in value.split()])
+
+    faces = np.asarray(faces)
+    verts = np.asarray(verts) 
+    return faces, verts
---
+++ tests/test_wavefront.py
@@ -0,0 +1,14 @@
+"""Test out the Wavefront reading and writing
+"""
+
+from point_cloud import wavefront
+
+# TODO Implement test cases
+def test_reader_works_properly():
+    """Test that number of faces/verts matches Euler's constraint
+    """
+    pass
+
+def test_reader_matches():
+    """Compare faces/verts to Matlab mat file?"""
+    pass
