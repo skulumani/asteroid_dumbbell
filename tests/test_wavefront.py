e713e825b405746ccf9fd7eb022f6cdc4300e655
Shankar Kulumani <shanks.k@gmail.com>
2017-10-24 09:21:09 -0400

    Combine all wavefront functions to one module

--- tests/test_wavefront.py
+++ tests/test_wavefront.py
@@ -1,13 +1,67 @@
-"""Test out the Wavefront reading and writing
-"""
-
-from point_cloud import wavefront
+import vtk
 import numpy as np
+from point_cloud import wavefront
 import scipy.io
 import pdb
 
+def test_vtk_major_version():
+    np.testing.assert_allclose(vtk.VTK_MAJOR_VERSION, 7)
 
+class TestVTKReadingOBJFilesItokawaLow():
+    filename = './data/shape_model/ITOKAWA/itokawa_low.obj'
+    vtkPolyData = wavefront.read_obj_to_polydata(filename)
+    vtk_vertices, vtk_faces = wavefront.vtk_poly_to_numpy(vtkPolyData)
+    wavefront_verts, wavefront_faces = wavefront.read_obj(filename)
 
+    def test_numpy_vertices_equal(self):
+        """Convert both to numpy representation and compare
+        """
+        np.testing.assert_allclose(self.vtk_vertices, self.wavefront_verts)
+
+    def test_numpy_faces_equal(self):
+        """Convert both to polydata representation and compare
+        """
+        np.testing.assert_allclose(self.vtk_faces, self.wavefront_faces)
+
+    def test_number_of_faces_equal(self):
+        np.testing.assert_allclose(self.vtkPolyData.GetNumberOfPolys(), self.wavefront_faces.shape[0])
+
+    def test_number_of_vertices_equal(self):
+        np.testing.assert_allclose(self.vtkPolyData.GetNumberOfPoints(), self.wavefront_verts.shape[0])
+
+class TestVTKReadingOBJFilesItokawaHigh():
+    filename = './data/shape_model/ITOKAWA/itokawa_high.obj'
+    vtkPolyData = wavefront.read_obj_to_polydata(filename)
+    vtk_vertices, vtk_faces = wavefront.vtk_poly_to_numpy(vtkPolyData)
+    wavefront_verts, wavefront_faces = wavefront.read_obj(filename)
+
+    def test_numpy_vertices_equal(self):
+        """Convert both to numpy representation and compare
+        """
+        np.testing.assert_allclose(self.vtk_vertices, self.wavefront_verts)
+
+    def test_numpy_faces_equal(self):
+        """Convert both to polydata representation and compare
+        """
+        np.testing.assert_allclose(self.vtk_faces, self.wavefront_faces)
+
+    def test_number_of_faces_equal(self):
+        np.testing.assert_allclose(self.vtkPolyData.GetNumberOfPolys(), self.wavefront_faces.shape[0])
+
+    def test_number_of_vertices_equal(self):
+        np.testing.assert_allclose(self.vtkPolyData.GetNumberOfPoints(), self.wavefront_verts.shape[0])
+
+class TestCompareVTKAndWavefrontWritingOBJ():
+    """Here we test that we can write to OBJ files using both VTK and Wavefront modules
+    """
+    filename = './data/shape_model/ITOKAWA/itokawa_low.obj'
+
+    vtkPolyData = wavefront.read_obj_to_polydata(filename)
+    input_vertices, input_faces = wavefront.vtk_poly_to_numpy(vtkPolyData)
+    
+    # write OBJ file using both VTK and Wavefront
+
+    # ensure the files are the same by reading
 # TODO: Compare with the matlab file with faces/vertices
 
 def test_reader_matches():
