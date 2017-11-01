f7cdb055676bd541418208266d4cbd627a65f7cc
Shankar Kulumani <shanks.k@gmail.com>
2017-11-01 17:36:25 -0400

    Can now plot the iso cut planes

--- examples/gravity_potential.py
+++ examples/gravity_potential.py
@@ -6,17 +6,28 @@ import pdb
 from mayavi import mlab
 import mayavi.api
 
-def define_grid(ast):
+def define_grid(ast, density):
     # generate a grid around the asteroid
-    nx, ny, nz = (5,5,5)
+    nx, ny, nz = (5, 5, 5)
     xmin, xmax = -ast.axes[0], ast.axes[0]
     ymin, ymax = -ast.axes[1], ast.axes[1]
     zmin, zmax = -ast.axes[2], ast.axes[2]
 
     xg, yg, zg = np.mgrid[ xmin:xmax:5j, ymin:ymax:5j, zmin:zmax:5j]
     
-    return 
-def generate_scalar_data(ast):
+    grid = {'xg': xg, 'yg':yg, 'zg':zg,
+            'nx': nx, 'ny':ny, 'nz':nz}
+    return grid
+
+def generate_scalar_data(ast, grid):
+    nx = grid['nx']
+    ny = grid['ny']
+    nz = grid['nz']
+    
+    xg = grid['xg']
+    yg = grid['yg']
+    zg = grid['zg']
+
     Ug_array = np.zeros((nx, ny, nz))
     # fidn the potential at each point
     for ii in range(nx):
@@ -31,26 +42,48 @@ def generate_scalar_data(ast):
 
     return Ug_array
 
-def visualize_data(ast, Ug_array):
+def visualize_data(ast, Ug_array, grid):
 
+    nx = grid['nx']
+    ny = grid['ny']
+    nz = grid['nz']
+    
+    xg = grid['xg']
+    yg = grid['yg']
+    zg = grid['zg']
     # plot some contour plots in mayavi
-    engine = mayavi.api.Engine()
-    engine.start()
+    # engine = mayavi.api.Engine()
+    # engine.start()
+    # scene = engine.scenes[0]
 
-    potential_fig = mlab.figure(figure=None, engine=engine)
+    potential_fig = mlab.figure(figure=None)
     mesh = wavefront.draw_polyhedron_mayavi(ast.V, ast.F, potential_fig)                
     # contour plot
     # contour3d = mlab.contour3d(xg, yg, zg, Ug_array, figure=potential_fig)
     # volume rendering
-    mlab.pipeline.volume(mlab.pipeline.scalar_field(xg, yg, zg,Ug_array))
-    scene = engine.scenes[0]
-
+    # volume = mlab.pipeline.volume(mlab.pipeline.scalar_field(Ug_array))
+    f = mlab.pipeline.scalar_field(xg, yg, zg, Ug_array)
+    # v = mlab.pipeline.volume(f, vmin=0, vmax=0.2)
+    
+    # cut planes
+    mlab.pipeline.image_plane_widget(f, 
+                                     plane_orientation='x_axes',
+                                     slice_index=nx/2)
+    mlab.pipeline.image_plane_widget(f, 
+                                     plane_orientation='y_axes',
+                                     slice_index=ny/2)
+    mlab.pipeline.image_plane_widget(f, 
+                                     plane_orientation='z_axes',
+                                     slice_index=nz/2)
+    mlab.outline()
+    mlab.axes()
     mlab.show()
 
 if __name__ == '__main__':
-    ast = asteroid.Asteroid('itokawa', 128, 'obj')
-    Ug_array = generate_scalar_data()
-    visualize_data(Ug_array)
+    ast = asteroid.Asteroid('itokawa', 0, 'obj')
+    grid = define_grid(ast)
+    Ug_array = generate_scalar_data(ast, grid)
+    visualize_data(ast, Ug_array, grid)
 # handle for plot
 # iso_surface = scene.children[0].children[0].children[0]
 # iso_surface.contour.print_traits()
