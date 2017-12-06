fddb36c4f6e352587d40a4856ea28ff04697323e
Shankar Kulumani <shanks.k@gmail.com>
2017-12-06 17:05:16 -0500

    Update lidar object

--- point_cloud/lidar.py
+++ point_cloud/lidar.py
@@ -1,3 +1,5 @@
+import numpy as np
+import itertools, pdb
 
 class Lidar(object):
     """LIDAR object
@@ -10,8 +12,10 @@ class Lidar(object):
 
     """
 
-    def __init__(self, cam_axis=np.array([1, 0, 0]),
-                 fov=np.deg2rad(np.array([7, 7])), sigma=0.2):
+    def __init__(self, view_axis=np.array([1, 0, 0]),
+                 up_axis=np.array([0, 0, 1]),
+                 fov=np.deg2rad(np.array([7, 7])), sigma=0.2,
+                 dist=1, num_step=3):
         """Initialize the object
 
         cam_axis : define the pointing direction of center of LIDAR in the
@@ -22,15 +26,31 @@ class Lidar(object):
         sigma : 3 sigma uncertainty in depth measurements
         """
         
-        # need to define both a look direciton and an up direction, which are orthogonal
 
+        right_axis = np.cross(view_axis, up_axis)
+
+        H = np.tan(fov[1]/2) * dist
+        W = np.tan(fov[0]/2) * dist
+        
+        # center of view fustrum
+        c = view_axis * dist
+        
+        # corners of the fustrum
+        hsteps = np.linspace(-H, H, num_step)
+        wsteps = np.linspace(-W, W, num_step)
+        
+        self.lidar_arr = np.zeros((num_step**2, 3))
         # define all the unit vectors for the sensor
+        for ii, (h, w) in enumerate(itertools.product(hsteps, wsteps)):
+            lidar_vec = c + h * up_axis + w * right_axis
+            lidar_arr[ii, :] = lidar_vec / np.linalg.norm(lidar_vec)
 
-        # need to rotate the camera axis +/- FOV in horizontal and vertical direction
+        # save things to object
+        self.lidar_arr = lidar_arr
+        self.up_axis = up_axis
+        self.view_axis = view_axis
+        self.right_axis = right_axis
 
-        # outpupt a bunch of unit vectors to the object
-        pass
-    
     def rotate_fov(self, R_body2inertial):
         """Rotate the entire FOV by a given rotation matrix
         """
