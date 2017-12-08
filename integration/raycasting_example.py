5bb77d79f342132b05abdb9c020504e64167425a
Shankar Kulumani <shanks.k@gmail.com>
2017-12-08 15:08:48 -0500

    Fix imports

--- integration/raycasting_example.py
+++ integration/raycasting_example.py
@@ -1,15 +1,16 @@
 """Script to do some raycasting with the simulated lidar
 """
 
-from point_cloud import wavefront, lidar, raycaster
+from point_cloud import wavefront, raycaster
 import numpy as np
 from visualization import graphics
+import pdb
 
 filename = './data/shape_model/ITOKAWA/itokawa_high.obj'
 polydata = wavefront.read_obj_to_polydata(filename)
 
 caster = raycaster.RayCaster(polydata)
-sensor = lidar.Lidar(view_axis=np.array([-1, 0, 0]))
+sensor = raycaster.Lidar(view_axis=np.array([-1, 0, 0]))
 
 # need to translate the sensor and give it a pointing direction
 pos = np.array([1, 0, 0])
