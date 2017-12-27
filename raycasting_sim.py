cf16aa35902bab1de05936e7d25e39c5bb38eba0
Shankar Kulumani <shanks.k@gmail.com>
2017-12-27 11:58:59 -0500

    Fix raycasting simulation

--- raycasting_sim.py
+++ raycasting_sim.py
@@ -25,15 +25,15 @@ initial_w = np.array([0, 0, 0])
 initial_state = np.hstack((initial_pos, initial_vel, initial_R, initial_w))
 
 # try both a controlled and uncontrolled simulation
-t, istate, astate, bstate = eoms.inertial_eoms_driver(initial_state,
-                                                      time, ast, dum)
+# t, istate, astate, bstate = eoms.inertial_eoms_driver(initial_state, time, ast, dum)
 
+state = eoms.sim_controlled_driver(time, initial_state, ast, dum)
 # plot the simulation
 # plotting.animate_inertial_trajectory(t, istate, ast, dum)
 # plotting.plot_inertial(t, istate, ast, dum, fwidth=1)
 
-# animation in mayavi
+# TODO: animation in mayavi
 mfig = graphics.mayavi_figure()
 
 graphics.draw_polyhedron_mayavi(ast.V, ast.F, mfig)
-graphics.mayavi_plot_trajectory(mfig, istate[:, 0:3])
+graphics.mayavi_plot_trajectory(mfig, state[:, 0:3])
