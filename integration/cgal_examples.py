64d994abb1cf7776704c69e601f9bbff705b0c3f
Shankar Kulumani <shanks.k@gmail.com>
2018-01-20 21:54:15 -0500

    add another example

--- integration/cgal_examples.py
+++ integration/cgal_examples.py
@@ -5,7 +5,7 @@ https://doc.cgal.org/latest/Manual/tutorial_hello_world.html
 
 import numpy as np
 import CGAL.CGAL_Kernel as kernel
-import CGAL
+from CGAL import CGAL_Convex_hull_2
 
 def points_and_segments():
     """Example from CGAL Hello world
@@ -27,3 +27,25 @@ def points_and_segments():
     print("sqdist(seg(p,q, m)) = {}".format(kernel.squared_distance(s, m)))
     print("s = {}".format(s))
 
+def convex_hull_2d():
+    """Convex hull for two dimensions
+    """
+    points = []
+    points.append(kernel.Point_2(0, 0))
+    points.append(kernel.Point_2(1, 0))
+    points.append(kernel.Point_2(0, 1))
+    points.append(kernel.Point_2(1, 1,))
+    points.append(kernel.Point_2(0.5, 0.5))
+    points.append(kernel.Point_2(0.25, 0.25))
+
+    result = []
+    CGAL_Convex_hull_2.convex_hull_2(points, result)
+
+    for p in result:
+        print(p)
+    
+    points.append(kernel.Point_2(2, 2))
+    n = kernel.Point_2()
+
+    CGAL_Convex_hull_2.ch_n_point(points, n)
+    print(n)
