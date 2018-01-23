3f834e10b0e4e0bcad53eed4618b8be81c5e7dad
Shankar Kulumani <shanks.k@gmail.com>
2018-01-23 09:20:00 -0500

    Start on testing out the edge distance function

--- integration/tests/test_wavefront.py
+++ integration/tests/test_wavefront.py
@@ -23,6 +23,26 @@ def test_normal_face_plot():
         graphics.mayavi_addLine(mfig, p1, u + p1)
 
 def test_closest_edge_plot():
+    ast = asteroid.Asteroid('castalia', 256, 'mat')
+    v, f = wavefront.read_obj('./integration/cube.obj')
+    ast = ast.loadmesh(v, f, 'cube')
+    
+
+    pt = np.array([0.5, 0, 0.1])
+    D, P, F, V = wavefront.distance_to_edges(pt, v, f, 
+                                             ast.asteroid_grav['normal_face'])
+
+    # draw the mayavi figure
+    mfig = graphics.mayavi_figure()
+    graphics.mayavi_addMesh(mfig, v, f)
+
+    graphics.mayavi_addPoint(mfig, pt, radius=0.1, color=(0, 1, 0))
+    graphics.mayavi_addPoint(mfig, P, radius=0.1, color=(1, 0, 0))
+    
+    # different color for each face
+    for f_ind in F:
+        face_verts = v[f[f_ind,:],:]
+        graphics.mayavi_addMesh(mfig, face_verts,[(0, 1, 2)], color=tuple(np.random.rand(3)))
     pass
 
 def test_closest_vertex_plot_asteroid():
@@ -67,9 +87,10 @@ def test_closest_vertex_plot_cube():
 
 # TODO Add a funciton to plot pt, and closest vertex, edge, and face in a color
 if __name__ == "__main__":
+    test_closest_edge_plot()
+    # test_closest_vertex_plot_cube()
+    # test_closest_vertex_plot_asteroid()
 
-    test_closest_vertex_plot_cube()
-    test_closest_vertex_plot_asteroid()
     # test_normal_face_plot()
 
 
