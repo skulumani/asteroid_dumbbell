eb30ef813be6217c50d9a26433931dd83c487dd4
Shankar Kulumani <shanks.k@gmail.com>
2018-01-22 21:52:46 -0500

    Add some integration tests
    Plot the minimum distance vertex and associated faces

--- integration/tests/test_wavefront.py
+++ integration/tests/test_wavefront.py
@@ -1,9 +1,11 @@
 """Module to test out some wavefront function with plots
 """
+import pdb
 import numpy as np
 
 from point_cloud import wavefront
 from visualization import graphics
+from dynamics import asteroid
 
 def test_normal_face_plot():
     """Plot the normals to the faces on a mesh and view in Mayavi
@@ -20,8 +22,54 @@ def test_normal_face_plot():
         graphics.mayavi_addPoint(mfig, p1, radius=0.1, color=(1, 0, 0))
         graphics.mayavi_addLine(mfig, p1, u + p1)
 
+def test_closest_edge_plot():
+    pass
+
+def test_closest_vertex_plot_asteroid():
+    ast = asteroid.Asteroid('castalia', 256, 'mat')
+    v =  ast.asteroid_grav['V']
+    f = ast.asteroid_grav['F']
+    pt = np.array([1, 0, 0])
+    D, P, F, V = wavefront.distance_to_vertices(pt, v, f, 
+                                                ast.asteroid_grav['normal_face'])
+
+    # draw the mayavi figure
+    mfig = graphics.mayavi_figure()
+    graphics.mayavi_addMesh(mfig, v, f)
+
+    graphics.mayavi_addPoint(mfig, pt, radius=0.1, color=(0, 1, 0))
+    graphics.mayavi_addPoint(mfig, P, radius=0.1, color=(1, 0, 0))
+    
+    # different color for each face
+    for f_ind in F:
+        face_verts = v[f[f_ind,:],:]
+        graphics.mayavi_addMesh(mfig, face_verts,[(0, 1, 2)], color=tuple(np.random.rand(3)))
+
+def test_closest_vertex_plot_cube():
+    ast = asteroid.Asteroid('castalia', 256, 'mat')
+    v, f = wavefront.read_obj('./integration/cube.obj')
+    ast = ast.loadmesh(v, f, 'cube')
+    pt = np.array([2, 0, 0])
+    D, P, F, V = wavefront.distance_to_vertices(pt, v, f, 
+                                                ast.asteroid_grav['normal_face'])
+
+    # draw the mayavi figure
+    mfig = graphics.mayavi_figure()
+    graphics.mayavi_addMesh(mfig, v, f)
+
+    graphics.mayavi_addPoint(mfig, pt, radius=0.1, color=(0, 1, 0))
+    graphics.mayavi_addPoint(mfig, P, radius=0.1, color=(1, 0, 0))
+    
+    # different color for each face
+    for f_ind in F:
+        face_verts = v[f[f_ind,:],:]
+        graphics.mayavi_addMesh(mfig, face_verts,[(0, 1, 2)], color=tuple(np.random.rand(3)))
+
 # TODO Add a funciton to plot pt, and closest vertex, edge, and face in a color
 if __name__ == "__main__":
-    test_normal_face_plot()
+
+    test_closest_vertex_plot_cube()
+    test_closest_vertex_plot_asteroid()
+    # test_normal_face_plot()
 
 
