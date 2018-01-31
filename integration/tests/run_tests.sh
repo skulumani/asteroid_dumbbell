aea58f653184ffb43da2e18847e30977f2f4a706
Shankar Kulumani <shanks.k@gmail.com>
2018-01-31 10:38:36 -0500

    Try to get script to run

--- integration/tests/run_tests.sh
+++ integration/tests/run_tests.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+set -e 
+set -x
+
+DIR=$(pwd)
+echo ${DIR}
 source activate asteroid
-python test_graphics.py
-python test_polyhedron_functions.py
-python test_searching.py
-python test_wavefront.py
+
+python "${DIR}/test_graphics.py"
+# python test_polyhedron_functions.py
+# python test_searching.py
+# python test_wavefront.py
