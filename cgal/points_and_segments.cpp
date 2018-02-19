63699c572a9a5ec4b966c3b63c626d4b2e65d96a
Shankar Kulumani <shanks.k@gmail.com>
2018-02-19 02:16:31 -0500

    Complete points and segments tutorial

--- cgal/points_and_segments.cpp
+++ cgal/points_and_segments.cpp
@@ -12,5 +12,29 @@ int main()
 
     std::cout << "p = " << p << std::endl;
     std::cout << "q = " << q.x() << " " << q.y() << std::endl;
+
+    std::cout << "sqdist(p, q) = " << CGAL::squared_distance(p, q) << std::endl;
+
+    Segment_2 s(p, q);
+    Point_2 m(5, 9);
+
+    std::cout << "m = " << m << std::endl;
+    std::cout << "sqdist(Segment_2(p, q), m) = " 
+              << CGAL::squared_distance(s, m) << std::endl;
+
+    std::cout << "p, q, and m ";
+    switch (CGAL::orientation(p, q, m)){
+    case CGAL::COLLINEAR:
+        std::cout << "are colinear\n";
+        break;
+    case CGAL::LEFT_TURN:
+        std::cout << "make a left turn\n";
+        break;
+    case CGAL::RIGHT_TURN:
+        std::cout << "make a right turn\n";
+        break;
+    }
+
+    std::cout << " midpoint(p, q) = " << CGAL::midpoint(p,q) << std::endl;
     return 0;
 }
