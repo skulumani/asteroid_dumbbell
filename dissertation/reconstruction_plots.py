6a4c1ebea052d8d049bbdebc59695eee5b5e10d6
Shankar Kulumani <shanks.k@gmail.com>
2018-02-19 19:49:06 -0500

    Add function to generate plots for radius based testing

---
+++ dissertation/reconstruction_plots.py
@@ -0,0 +1,78 @@
+"""Plots for reconstruction of a mesh from point cloud
+"""
+import pdb
+import numpy as np
+import warnings
+import os
+
+from point_cloud import wavefront
+from visualization import graphics
+from kinematics import sphere
+
+
+view = {'azimuth': 16.944197132093564, 'elevation': 66.34177792039738,
+        'distance': 2.9356815748114435, 
+        'focalpoint': np.array([0.20105769, -0.00420018, -0.016934045])}
+
+def cube_into_sphere(img_path):
+    """Transform a cube into a sphere
+    """
+    vc, fc = wavefront.read_obj('./integration/cube.obj')
+    vs, fs = wavefront.ellipsoid_mesh(2, 2, 2, density=10, subdivisions=0)
+
+    mfig = graphics.mayavi_figure()
+    mesh = graphics.mayavi_addMesh(mfig, vc,fc)
+    ms = mesh.mlab_source
+    index = 0
+    for ii in range(5):
+        for jj, pt in enumerate(vs):
+            index += 1
+            filename = os.path.join(img_path, 'cube_sphere_' + str(index).zfill(6) + '.jpg')
+            graphics.mlab.savefig(filename, magnification=4)
+            vc, fc = wavefront.radius_mesh_incremental_update(pt, vc, fc)
+            ms.reset(x=vc[:, 0], y=vc[:, 1], z=vc[:, 2], triangles=fc)
+            graphics.mayavi_addPoint(mfig, pt)
+        
+        vc, fc = wavefront.mesh_subdivide(vc, fc, 1)
+        ms.reset(x=vc[:, 0], y=vc[:, 1], z=vc[:, 2], triangles=fc)
+
+    return 0
+
+def sphere_into_ellipsoid(img_path):
+    """See if we can turn a sphere into an ellipse by changing the radius of
+    vertices
+    
+    The point cloud (ellipse) should have more points than the initial mesh.
+    When the intial mesh is coarse the resulting mesh will also be heavily faceted, but this will avoid the big holes, and large changes in depth
+    """
+
+    # define the sphere
+    vs, fs = wavefront.ellipsoid_mesh(1, 1, 1, density=10, subdivisions=0)
+    ve, fe = wavefront.ellipsoid_mesh(2, 3, 4, density=10, subdivisions=2)
+    
+    mfig = graphics.mayavi_figure()
+    mesh = graphics.mayavi_addMesh(mfig, vs, fs)
+    ms = mesh.mlab_source
+    index = 0
+    # in a loop add each vertex of the ellipse into the sphere mesh
+    for jj in range(2):
+        for ii, pt in enumerate(ve):
+            index +=1
+            filename = os.path.join(img_path, 'sphere_ellipsoid_' + str(index).zfill(6) + '.jpg')
+            graphics.mlab.savefig(filename, magnification=4)
+            vs, fs = wavefront.radius_mesh_incremental_update(pt, vs,fs)
+            ms.reset(x=vs[:,0], y=vs[:,1], z=vs[:,2], triangles=fs)
+            graphics.mayavi_addPoint(mfig, pt)
+    
+        vs, fs = wavefront.mesh_subdivide(vs, fs,  1)
+        ms.reset(x=vs[:,0], y=vs[:,1], z=vs[:,2], triangles=fs)
+
+    return 0
+
+if __name__ == "__main__":
+    img_path = '/tmp/mayavi_figure'
+    if not os.path.exists(img_path):
+        os.makedirs(img_path)
+
+    # cube_into_sphere(img_path)
+    sphere_into_ellipsoid(img_path)
