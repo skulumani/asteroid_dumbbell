a98b408a69f5f5ed90854ba37baa474fc4b6dbcf
Shankar Kulumani <shanks.k@gmail.com>
2018-02-24 13:21:09 -0500

    Add a class and access for python

--- examples/boost-python/hello-world/hello.cpp
+++ examples/boost-python/hello-world/hello.cpp
@@ -1,12 +1,31 @@
+#include <boost/python.hpp>
+
+// A function
 char const* greet()
 {
     return "hello world!";
 }
 
-#include <boost/python.hpp>
+// Now we a class and member functions
+struct World
+{
+    void set(std::string msg)
+    {
+        this->msg = msg;
+    }
+    std::string greet()
+    {
+        return msg;
+    }
+    std::string msg;
+};
 
 BOOST_PYTHON_MODULE(hello)
 {
     using namespace boost::python;
     def("greet", greet);
+
+    class_<World>("World")
+        .def("greet", &World::greet)
+        .def("set", &World::set);
 }
