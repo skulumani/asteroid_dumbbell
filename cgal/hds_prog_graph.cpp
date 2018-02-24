355214af45398e748dfd575b0734c2a635a6bb71
Shankar Kulumani <shanks.k@gmail.com>
2018-02-23 19:01:20 -0500

    Add some cgal examples

---
+++ cgal/hds_prog_graph.cpp
@@ -0,0 +1,16 @@
+#include <CGAL/HalfedgeDS_min_items.h>
+#include <CGAL/HalfedgeDS_default.h>
+#include <CGAL/HalfedgeDS_decorator.h>
+
+// no traits needed
+typedef CGAL::HalfedgeDS_default<int, CGAL::HalfedgeDS_min_items> HDS;
+typedef CGAL::HalfedgeDS_decorator<HDS> Decorator;
+
+int main()
+{
+    HDS hds;
+    Decorator decorator(hds);
+    decorator.create_loop();
+    CGAL_assertion(decorator.is_valid());
+    return 0;
+}
