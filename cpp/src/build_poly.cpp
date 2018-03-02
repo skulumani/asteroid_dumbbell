77d36cc0a30c75cdcb7cc3c7aec86b0a98fbb9e4
Shankar Kulumani <shanks.k@gmail.com>
2018-03-02 14:57:51 -0500

    Eigen to polyhedron_3 builder is complete

--- cpp/src/build_poly.cpp
+++ cpp/src/build_poly.cpp
@@ -11,38 +11,47 @@
 #include <fstream>
 #include <vector>
 
+typedef CGAL::Simple_cartesian<double>     Kernel;
+typedef CGAL::Polyhedron_3<Kernel>         Polyhedron;
+typedef Polyhedron::HalfedgeDS             HalfedgeDS;<Paste>
+
 // declaration for the polyhedron builder
-template<typename HDS, typename Derived> 
+template<typename HDS, typename VectorType, typename IndexType> 
 class Polyhedron_builder : public CGAL::Modifier_base<HDS> {
     public:
-        Polyhedron_builder(Eigen::PlainObjectBase<Derived> &V, Eigen::PlainObjectBase<Derived> &F);
-        void operator() (HDS &hds);
-};
+        Eigen::PlainObjectBase<VectorType> &V;
+        Eigen::PlainObjectBase<IndexType> &F;
 
-// definition for polyhedron builder
-//
-template<typename HDS> 
-class Build_triangle : public CGAL::Modifier_base<HDS> {
-    public:
-        Build_triangle () {}
+        Polyhedron_builder(Eigen::PlainObjectBase<VectorType> &V_input, Eigen::PlainObjectBase<IndexType> &F_input) : V(V_input), F(F_input) {};
 
-        void operator() (HDS& hds) {
-            // postcondition hds is a valid polyhedral surface
-            CGAL::Polyhedron_incremental_builder_3<HDS> B(hds, true);
-            B.begin_surface(3, 1, 6);
+        void operator() (HDS &hds) {
+
             typedef typename HDS::Vertex Vertex;
             typedef typename Vertex::Point Point;
-            B.add_vertex(Point(0, 0, 0));
-            B.add_vertex(Point(1, 0, 0));
-            B.add_vertex(Point(0, 1, 0));
-            B.begin_facet();
-            B.add_vertex_to_facet(0);
-            B.add_vertex_to_facet(1);
-            B.add_vertex_to_facet(2);
-            B.end_facet();
+
+            // create the cgal incremental builder
+            CGAL::Polyhedron_incremental_builder_3<HDS> B(hds, true);
+            // initialize with #v, #f, #half-edges (optional)
+            B.begin_surface(V.rows(), F.rows());
+
+            // add all of the vertices
+            for (int ii = 0; ii < V.rows(); ++ii) {
+                B.add_vertex(Point(V(ii, 0), V(ii, 1), V(ii, 2)));
+            }
+            // add all of the faces
+            for (int ii = 0; ii < F.rows(); ++ii) {
+                B.begin_facet();
+                B.add_vertex_to_facet(F(ii, 0));
+                B.add_vertex_to_facet(F(ii, 1));
+                B.add_vertex_to_facet(F(ii, 2));
+                B.end_facet()
+            }
             B.end_surface();
         }
 };
+
+
+
 
 int main(int argc, char* argv[]) {
     InputParser input(argc, argv);
@@ -66,6 +75,7 @@ int main(int argc, char* argv[]) {
             Eigen::MatrixXi F;
             vector_array_to_eigen(vector_V, V);
             vector_array_to_eigen(vector_F, F);
+
         }
          
     }  // input file is closed when leaving the scope
