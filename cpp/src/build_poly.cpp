ecd6d4fbf8285264d88c5fd0c93dd1d61de5e0cc
Shankar Kulumani <shanks.k@gmail.com>
2018-03-02 15:44:50 -0500

    Seperate declaration and defintiion

--- cpp/src/build_poly.cpp
+++ cpp/src/build_poly.cpp
@@ -24,34 +24,36 @@ class Polyhedron_builder : public CGAL::Modifier_base<HDS> {
         Eigen::MatrixXi &F;
 
         Polyhedron_builder(Eigen::MatrixXd &V_input, Eigen::MatrixXi &F_input) : V(V_input), F(F_input) {};
+		
+		void operator()(HDS &hds);
 
-        void operator() (HDS &hds) {
-
-            typedef typename HDS::Vertex Vertex;
-            typedef typename Vertex::Point Point;
-
-            // create the cgal incremental builder
-            CGAL::Polyhedron_incremental_builder_3<HDS> B(hds, true);
-            // initialize with #v, #f, #half-edges (optional)
-            B.begin_surface(V.rows(), F.rows());
-
-            // add all of the vertices
-            for (int ii = 0; ii < V.rows(); ++ii) {
-                B.add_vertex(Point(V(ii, 0), V(ii, 1), V(ii, 2)));
-            }
-            // add all of the faces
-            for (int ii = 0; ii < F.rows(); ++ii) {
-                B.begin_facet();
-                B.add_vertex_to_facet(F(ii, 0));
-                B.add_vertex_to_facet(F(ii, 1));
-                B.add_vertex_to_facet(F(ii, 2));
-                B.end_facet();
-            }
-            B.end_surface();
-        }
 };
 
+template<typename HDS>
+void Polyhedron_builder<HDS>::operator() (HDS &hds) {
 
+	typedef typename HDS::Vertex Vertex;
+	typedef typename Vertex::Point Point;
+
+	// create the cgal incremental builder
+	CGAL::Polyhedron_incremental_builder_3<HDS> B(hds, true);
+	// initialize with #v, #f, #half-edges (optional)
+	B.begin_surface(V.rows(), F.rows());
+
+	// add all of the vertices
+	for (int ii = 0; ii < V.rows(); ++ii) {
+		B.add_vertex(Point(V(ii, 0), V(ii, 1), V(ii, 2)));
+	}
+	// add all of the faces
+	for (int ii = 0; ii < F.rows(); ++ii) {
+		B.begin_facet();
+		B.add_vertex_to_facet(F(ii, 0));
+		B.add_vertex_to_facet(F(ii, 1));
+		B.add_vertex_to_facet(F(ii, 2));
+		B.end_facet();
+	}
+	B.end_surface();
+}
 
 
 int main(int argc, char* argv[]) {
