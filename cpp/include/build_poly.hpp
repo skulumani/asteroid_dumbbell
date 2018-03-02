19b41a5141e1ea44924605cc6a4011134889d87d
Shankar Kulumani <shanks.k@gmail.com>
2018-03-02 16:46:13 -0500

    Move definition to the header as well

--- cpp/include/build_poly.hpp
+++ cpp/include/build_poly.hpp
@@ -1,6 +1,8 @@
 #ifndef BUILD_POLY_H
 #define BUILD_POLY_H
 
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Polyhedron_incremental_builder_3.h>
 #include <CGAL/Polyhedron_3.h>
 
 // declaration for the polyhedron builder
@@ -12,7 +14,30 @@ class Polyhedron_builder : public CGAL::Modifier_base<HDS> {
 
         Polyhedron_builder(Eigen::MatrixXd &V_input, Eigen::MatrixXi &F_input) : V(V_input), F(F_input) {};
 		
-		void operator()(HDS &hds);
+        void operator() (HDS &hds) {
+
+            typedef typename HDS::Vertex Vertex;
+            typedef typename Vertex::Point Point;
+
+            // create the cgal incremental builder
+            CGAL::Polyhedron_incremental_builder_3<HDS> B(hds, true);
+            // initialize with #v, #f, #half-edges (optional)
+            B.begin_surface(V.rows(), F.rows());
+
+            // add all of the vertices
+            for (int ii = 0; ii < V.rows(); ++ii) {
+                B.add_vertex(Point(V(ii, 0), V(ii, 1), V(ii, 2)));
+            }
+            // add all of the faces
+            for (int ii = 0; ii < F.rows(); ++ii) {
+                B.begin_facet();
+                B.add_vertex_to_facet(F(ii, 0));
+                B.add_vertex_to_facet(F(ii, 1));
+                B.add_vertex_to_facet(F(ii, 2));
+                B.end_facet();
+            }
+            B.end_surface();
+        }
 };
 
 #endif
--- cpp/src/build_poly_main.cpp
+++ cpp/src/build_poly_main.cpp
@@ -16,32 +16,6 @@ typedef CGAL::Simple_cartesian<double>     Kernel;
 typedef CGAL::Polyhedron_3<Kernel>         Polyhedron;
 typedef Polyhedron::HalfedgeDS             HalfedgeDS;
 
-template<typename HDS>
-void Polyhedron_builder<HDS>::operator() (HDS &hds) {
-
-	typedef typename HDS::Vertex Vertex;
-	typedef typename Vertex::Point Point;
-
-	// create the cgal incremental builder
-	CGAL::Polyhedron_incremental_builder_3<HDS> B(hds, true);
-	// initialize with #v, #f, #half-edges (optional)
-	B.begin_surface(V.rows(), F.rows());
-
-	// add all of the vertices
-	for (int ii = 0; ii < V.rows(); ++ii) {
-		B.add_vertex(Point(V(ii, 0), V(ii, 1), V(ii, 2)));
-	}
-	// add all of the faces
-	for (int ii = 0; ii < F.rows(); ++ii) {
-		B.begin_facet();
-		B.add_vertex_to_facet(F(ii, 0));
-		B.add_vertex_to_facet(F(ii, 1));
-		B.add_vertex_to_facet(F(ii, 2));
-		B.end_facet();
-	}
-	B.end_surface();
-}
-
 int main(int argc, char* argv[]) {
     InputParser input(argc, argv);
     if (input.option_exists("-h")) {
