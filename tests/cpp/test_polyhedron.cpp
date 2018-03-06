ef50f71d26fd4c73b899fe98afef112e8066a3e3
Shankar Kulumani <shanks.k@gmail.com>
2018-03-06 17:20:05 -0500

    Start of polyhedron test

---
+++ tests/cpp/test_polyhedron.cpp
@@ -0,0 +1,11 @@
+#include "polyhedron.hpp"
+
+#include "gtest/gtest.h"
+
+// TODO Test that we can build a polyhedron from an eigen V, F
+
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
