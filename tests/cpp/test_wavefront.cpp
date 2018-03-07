ef27619c9c0af56c8c86427f538ea47e288ff201
Shankar Kulumani <shanks.k@gmail.com>
2018-03-06 22:53:16 -0500

    Working on unit testing
    Eigen reading

--- tests/cpp/test_wavefront.cpp
+++ tests/cpp/test_wavefront.cpp
@@ -123,7 +123,38 @@ TEST_F(ReadOBJ, VectorOfVectorsElementsStream) {
 
 }
 
-TEST(VectorToEigen, VectorToEigen) {
+class EigenOBJ: public ::testing::Test {
+    protected:
+
+        EigenOBJ() {
+            input_stream.open(input_file);
+        }
+    std::vector<std::vector<double> > V_true {  { -0.5, -0.5, -0.5},
+                                             { -0.5, -0.5, 0.5},
+                                             { -0.5, 0.5, -0.5},
+                                             { -0.5, 0.5, 0.5},
+                                             { 0.5, -0.5, -0.5},
+                                             { 0.5, -0.5, 0.5},
+                                             { 0.5, 0.5, -0.5},
+                                             { 0.5, 0.5, 0.5}};
+    std::vector<std::vector<int> > F_true {
+                                          { 1, 7, 5},
+                                          { 1, 3, 7},
+                                          { 1, 4, 3},
+                                          { 1, 2, 4},
+                                          { 3, 8, 7},
+                                          { 3, 4, 8},
+                                          { 5, 7, 8},
+                                          { 5, 8, 6},
+                                          { 1, 5, 6},
+                                          { 1, 6, 2},
+                                          { 2, 6, 8},
+                                          { 2, 8, 4}};
+    const std::string input_file = "./integration/cube.obj";
+    std::ifstream input_stream;
+};
+// TODO Unit tests for reading from OBJ to eigen arrays
+TEST_F(EigenOBJ, VectorToEigen) {
     // initialize the truth vector of vectors
     std::vector<std::vector<double> > V_vector {  { -0.5, -0.5, -0.5},
                                              { -0.5, -0.5, 0.5},
@@ -149,7 +180,8 @@ TEST(VectorToEigen, VectorToEigen) {
     ASSERT_TRUE(V_actual.isApprox(V_array));
 }
 
-TEST(Testing, EigenArray) {
+
+TEST_F(EigenOBJ, EigenArray) {
     Eigen::Matrix<double, 8, 3> V_true;
     Eigen::Matrix<int, 12, 3> F_true;
 
