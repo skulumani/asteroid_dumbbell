dffdf15015efae3ba34a3438dec38e57957d7d60
Shankar Kulumani <shanks.k@gmail.com>
2018-03-09 17:21:39 -0500

    Newline

--- src/stats.cpp
+++ src/stats.cpp
@@ -1,11 +1,10 @@
 #include "stats.hpp"
-#include "mesh.hpp"
+/* #include "mesh.hpp" */
 
 #include <CGAL/Simple_cartesian.h>
 
 #include <iostream>
 #include <memory>
-
 
 void print_polyhedron_vertices(std::shared_ptr<MeshData> mesh) {
     std::copy (mesh->polyhedron.points_begin(), mesh->polyhedron.points_end(), std::ostream_iterator<CGAL::Simple_cartesian<double>::Point_3>(std::cout, "\n")); 
