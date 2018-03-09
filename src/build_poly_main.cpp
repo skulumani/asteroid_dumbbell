c4fc6c5114203d94c8bde9da7b00bf1d2e718131
Shankar Kulumani <shanks.k@gmail.com>
2018-03-09 08:53:17 -0500

    Testing out my mesh class

--- src/build_poly_main.cpp
+++ src/build_poly_main.cpp
@@ -36,9 +36,11 @@ int main(int argc, char* argv[]) {
 
         std::cout << "Now initializing a Poly object to store all of our data in a single structure/object" << std::endl;
 
-        Mesh P_eigen(cube.vertices, cube.faces);
-        Mesh P_string(input_file);
-         
+        Mesh mesh_eigen(cube.vertices, cube.faces);
+        Mesh mesh_string(input_file);
+    
+        std::cout << "V from mesh: \n" << mesh_string.vertices << std::endl;
+        std::cout << "F from mesh: \n" << mesh_string.faces << std::endl;
         // Now we'll extract the vertices adn faces
         /* auto eigen_arrays = P_eigen.get_arrays(); */
         
@@ -47,7 +49,6 @@ int main(int argc, char* argv[]) {
     }  // input file is closed when leaving the scope
     /* Eigen::MatrixXd V_poly; */
     /* Eigen::MatrixXi F_poly; */
-    std::cout << "Can extract V, F from a polyhedron now" << std::endl;
 
     /* polyhedron_to_eigen(P, V_poly, F_poly); */
     
