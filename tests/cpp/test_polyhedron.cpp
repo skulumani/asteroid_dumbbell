55106d20d5283d0eecdc9ba8c22fb075fbd170bb
Shankar Kulumani <shanks.k@gmail.com>
2018-03-09 08:53:29 -0500

    Getting mesh testing setup

--- tests/cpp/test_polyhedron.cpp
+++ tests/cpp/test_polyhedron.cpp
@@ -20,9 +20,29 @@ class TestPolyhedron: public ::testing::Test {
 
   TestPolyhedron() {
     // You can do set-up work for each test here.
-    read_flag = obj::read_to_eigen(input_file, V,  F);
-    Polyhedron_builder<HalfedgeDS> builder(V, F);
-    P.delegate(builder);
+    // initialize the mesh
+    Ve_true << -0.5,   -0.5, -0.5,
+            -0.5, -0.5, 0.5,
+            -0.5, 0.5,  -0.5,
+            -0.5, 0.5,  0.5,
+            0.5,  -0.5, -0.5,
+            0.5,  -0.5, 0.5,
+            0.5,  0.5,  -0.5,
+            0.5,  0.5,  0.5;
+
+    Fe_true << 1, 7, 5,
+            1, 3, 7,
+            1, 4, 3,
+            1, 2, 4,
+            3, 8, 7,
+            3, 4, 8,
+            5, 7, 8,
+            5, 8, 6,
+            1, 5, 6,
+            1, 6, 2,
+            2, 6, 8,
+            2, 8, 4;
+    Fe_true = Fe_true.array() - 1;
   }
 
   virtual ~TestPolyhedron() {
@@ -43,32 +63,30 @@ class TestPolyhedron: public ::testing::Test {
   }
 
   // Objects declared here can be used by all tests in the test case for Foo.
-    int read_flag;
-    Polyhedron P;
     const std::string input_file = "./integration/cube.obj";
-    Eigen::MatrixXd V;
-    Eigen::MatrixXi F;
+    Eigen::Matrix<double, 8, 3> Ve_true;
+    Eigen::Matrix<int, 12, 3> Fe_true;
 };
 
 // Tests that the Foo::Bar() method does Abc.
 TEST_F(TestPolyhedron, PolyhedronIsValid) {
-    ASSERT_EQ(P.is_valid(), 1);
+    Mesh mesh(input_file);
+    ASSERT_EQ(mesh.Poly.is_valid(), 1);
 }
 
-// Test number of vertices and faces
-TEST_F(TestPolyhedron, PolyhedronVertices) {
-    EXPECT_EQ(P.size_of_vertices(), 8);
-}
+/* // Test number of vertices and faces */
+/* TEST_F(TestPolyhedron, PolyhedronVertices) { */
+/*     Mesh mesh(input_file); */
+/*     EXPECT_EQ(mesh.Poly.size_of_vertices(), 8); */
+/* } */
 
-TEST_F(TestPolyhedron, PolyhedronFaces) {
-    EXPECT_EQ(P.size_of_facets(), 12);
-}
+/* TEST_F(TestPolyhedron, PolyhedronFaces) { */
+/*     Mesh mesh(input_file); */
+/*     EXPECT_EQ(mesh.Poly.size_of_facets(), 12); */
+/* } */
 
-TEST_F(TestPolyhedron, PolyhedronToEigen) {
-    Eigen::MatrixXd V_poly;
-    Eigen::MatrixXi F_poly;
-    
-    polyhedron_to_eigen(P, V_poly, F_poly);
-    EXPECT_EQ(V_poly.size(), V.size());
-    EXPECT_EQ(F_poly.size(), F.size());
-}
+/* TEST_F(TestPolyhedron, PolyhedronToEigen) { */
+/*     Mesh mesh(input_file); */
+/*     EXPECT_EQ(mesh.vertices.size(), Ve_true.size()); */
+/*     EXPECT_EQ(mesh.faces.size(), Fe_true.size()); */
+/* } */
