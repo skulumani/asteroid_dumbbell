084237f6439428d1abeda77ea83c29410753b1a5
Shankar Kulumani <shanks.k@gmail.com>
2018-03-11 19:23:10 -0400

    Working on creating eigen potential function

--- src/potential.cpp
+++ src/potential.cpp
@@ -13,16 +13,29 @@ void face_contribution_loop(Eigen::Vector3d r_v,  Eigen::MatrixXd V, Eigen::Matr
     std::cout << r_v << std::endl;
 }
 
-void laplacian_factor(Eigen::Matrix<double, Eigen::Dynamic, 3> r_v,
-                     Eigen::Matrix<int, Eigen::Dynamic, 1> Fa,
-                     Eigen::Matrix<int, Eigen::Dynamic, 1> Fb,
-                     Eigen::Matrix<int, Eigen::Dynamic, 1> Fc) {
+void laplacian_factor(Eigen::Array<double, Eigen::Dynamic, 3> r_v,
+                     Eigen::Array<int, Eigen::Dynamic, 1> Fa,
+                     Eigen::Array<int, Eigen::Dynamic, 1> Fb,
+                     Eigen::Array<int, Eigen::Dynamic, 1> Fc) {
     // form the ri, rj, rk arrays
-    Eigen::Matrix<int, Eigen::Dynamic, 3> ri, rj, rk;
+    Eigen::Array<double, Eigen::Dynamic, 3> ri, rj, rk;
+    Eigen::Array<double, 1, 3> ra, rb, rc;
 
     for (int ii = 0; ii < Fa.rows(); ++ii) {
-        std::cout << r_v.row(ii) << std::endl;
+        ra = r_v.row(Fa(ii));
+        rb = r_v.row(Fb(ii));
+        rc = r_v.row(Fc(ii));
+
+        ri.row(ii) << ra(0), ra(1), ra(2);
+        /* rj.row(ii) = rb; */
+        /* rk.row(ii) = rc; */
+
     }
+    std::cout << ri.row(0) << std::endl;
+    Eigen::Array<double, Eigen::Dynamic, 3> ri_norm, rj_norm, rk_norm;
+    
+    /* ri_norm = ri.pow(2); */
+    /* std::cout << ri << std::endl; */
 }
 
 PYBIND11_MODULE(polyhedron_potential, m) {
