dfd3d848ff44325e94fc25c28e60cbd5e637f314
Shankar Kulumani <shanks.k@gmail.com>
2018-03-11 20:56:07 -0400

    Working on laplacian function

--- src/potential.cpp
+++ src/potential.cpp
@@ -13,29 +13,49 @@ void face_contribution_loop(Eigen::Vector3d r_v,  Eigen::MatrixXd V, Eigen::Matr
     std::cout << r_v << std::endl;
 }
 
-void laplacian_factor(Eigen::Array<double, Eigen::Dynamic, 3> r_v,
-                     Eigen::Array<int, Eigen::Dynamic, 1> Fa,
-                     Eigen::Array<int, Eigen::Dynamic, 1> Fb,
-                     Eigen::Array<int, Eigen::Dynamic, 1> Fc) {
+void laplacian_factor(Eigen::Matrix<double, Eigen::Dynamic, 3> r_v,
+                     Eigen::Matrix<int, Eigen::Dynamic, 1> Fa,
+                     Eigen::Matrix<int, Eigen::Dynamic, 1> Fb,
+                     Eigen::Matrix<int, Eigen::Dynamic, 1> Fc) {
     // form the ri, rj, rk arrays
-    Eigen::Array<double, Eigen::Dynamic, 3> ri, rj, rk;
-    Eigen::Array<double, 1, 3> ra, rb, rc;
+    Eigen::Matrix<double, Eigen::Dynamic, 3> ri, rj, rk, rjrk_cross;
+    Eigen::Matrix<double, Eigen::Dynamic, 1> ri_norm, rj_norm, rk_norm;
+
+    ri.resize(Fa.rows(), 3);
+    rj.resize(Fb.rows(), 3);
+    rk.resize(Fc.rows(), 3);
+    rjrk_cross.resize(Fa.rows(), 3);
+    
+    ri_norm.resize(Fa.rows(), 1);
+    rj_norm.resize(Fb.rows(), 1);
+    rk_norm.resize(Fc.rows(), 1);
+
+    Eigen::Matrix<double, 1, 3> ra, rb, rc;
 
     for (int ii = 0; ii < Fa.rows(); ++ii) {
         ra = r_v.row(Fa(ii));
         rb = r_v.row(Fb(ii));
         rc = r_v.row(Fc(ii));
 
-        ri.row(ii) << ra(0), ra(1), ra(2);
-        /* rj.row(ii) = rb; */
-        /* rk.row(ii) = rc; */
+        ri.row(ii) = ra;
+        rj.row(ii) = rb;
+        rk.row(ii) = rc;
+
+        rjrk_cross.row(ii) = rj.row(ii).cross(rk.row(ii));
 
     }
-    std::cout << ri.row(0) << std::endl;
-    Eigen::Array<double, Eigen::Dynamic, 3> ri_norm, rj_norm, rk_norm;
+
+   
+    ri_norm = ri.array().pow(2).rowwise().sum().sqrt();
+    rj_norm = rj.array().pow(2).rowwise().sum().sqrt();
+    rk_norm = rk.array().pow(2).rowwise().sum().sqrt();
+    // dot product terms
+
+    // numerator and denonminator of atan2
+    Eigen::Matrix<double, Eigen::Dynamic, 1> num, den;
+    num = (ri.array() * rjrk_cross.array()).rowwise().sum();
     
-    /* ri_norm = ri.pow(2); */
-    /* std::cout << ri << std::endl; */
+    std::cout << num << std::endl;
 }
 
 PYBIND11_MODULE(polyhedron_potential, m) {
