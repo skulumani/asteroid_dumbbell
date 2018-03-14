22767a619bceedbd690b9d322036fdc1c7968647
Shankar Kulumani <shanks.k@gmail.com>
2018-03-14 15:33:42 -0400

    Now test out the edge factor

--- tests/cpp/test_potential.cpp
+++ tests/cpp/test_potential.cpp
@@ -7,6 +7,8 @@
 class TestPotential: public ::testing::Test {
     protected:
         TestPotential() {
+            state << 2, 0, 0;
+
             // You can do set-up work for each test here.
             // initialize the mesh
             Ve_true << -0.5,   -0.5, -0.5,
@@ -33,45 +35,136 @@ class TestPotential: public ::testing::Test {
             e3_vertex_map << 0, 4, 0, 6, 0, 2, 0, 3, 2, 6, 2, 7, 4, 7, 4, 5, 0,
                           5, 0, 1, 1, 7, 1, 3;
 
+            e1 << 1., 1., 0.,
+               0.,    1., 0.,
+               0.,    1., 1.,
+               0.,    0., 1.,
+               1.,    0., 1.,
+               0.,    0., 1.,
+               0.,    1., 0.,
+               0.,    1., 1.,
+               1.,    0., 0.,
+               1.,    0., 1.,
+               1.,    0., 0.,
+               1.,    1., 0.;
+
+            e2 << 0.,  -1., 0.,
+               1.,     0.,  0.,
+               0.,     0.,  -1.,
+               0.,     1.,  0.,
+               0.,     0.,  -1.,
+               1.,     0.,  0.,
+               0.,     0.,  1.,
+               0.,     -1., 0.,
+               0.,     0.,  1.,
+               -1.,    0.,  0.,
+               0.,     1.,  0.,
+               -1.,    0.,  0.;
+
+            e3 << -1., 0.,  0.,
+               -1.,    -1., 0.,
+               0.,     -1., 0.,
+               0.,     -1., -1.,
+               -1.,    0.,  0.,
+               -1.,    0.,  -1.,
+               0.,     -1., -1.,
+               0.,     0.,  -1.,
+               -1.,    0.,  -1.,
+               0.,     0.,  -1.,
+               -1.,    -1., 0.,
+               0.,     -1., 0.;
+
             w_face_true << 0.03808065, 0.02361574, 0.07694205, 0.07694205,
                         0.03808065, 0.02361574, -0.20033484, -0.20033484,
                         0.03808065, 0.02361574, 0.03808065,0.02361574;
+
+            L1_edge_true << 0.6907257,                    
+                         0.38976051,                    
+                         0.55840222,                    
+                         0.38976051,                    
+                         0.6907257,                    
+                         0.38976051,                    
+                         0.6223625,                    
+                         0.91098028,                    
+                         0.47882542,                    
+                         0.6907257,                    
+                         0.47882542,                    
+                         0.6907257;
+            L2_edge_true << 0.6223625,                    
+                         0.47882542,                    
+                         0.38976051,                    
+                         0.38976051,                    
+                         0.6223625,                    
+                         0.47882542,                    
+                         0.6223625,                    
+                         0.6223625,                    
+                         0.6223625,                    
+                         0.47882542,                    
+                         0.6223625,                    
+                         0.47882542;
+            L3_edge_true << 0.47882542,
+                         0.6907257,
+                         0.38976051,
+                         0.55840222,
+                         0.47882542,
+                         0.6907257,
+                         0.91098028,
+                         0.6223625,
+                         0.6907257,
+                         0.38976051,
+                         0.6907257,
+                         0.38976051;
         }
 
-  virtual ~TestPotential() {
-    // You can do clean-up work that doesn't throw exceptions here.
-  }
+        virtual ~TestPotential() {
+            // You can do clean-up work that doesn't throw exceptions here.
+        }
 
-  // If the constructor and destructor are not enough for setting up
-  // and cleaning up each test, you can define the following methods:
+        // If the constructor and destructor are not enough for setting up
+        // and cleaning up each test, you can define the following methods:
 
-  virtual void SetUp() {
-    // Code here will be called immediately after the constructor (right
-    // before each test).
-  }
+        virtual void SetUp() {
+            // Code here will be called immediately after the constructor (right
+            // before each test).
+        }
 
-  virtual void TearDown() {
-    // Code here will be called immediately after each test (right
-    // before the destructor).
-  }
+        virtual void TearDown() {
+            // Code here will be called immediately after each test (right
+            // before the destructor).
+        }
 
-  // Objects declared here can be used by all tests in the test case for Foo.
-    const std::string input_file = "./integration/cube.obj";
-    Eigen::Array<double, 8, 3> Ve_true;
-    Eigen::Array<int, 12, 3> Fe_true;
-    Eigen::Array<int, 12, 1> Fa, Fb, Fc;
-    Eigen::Array<double, 12, 3> e1, e2, e3;
-    Eigen::Array<int, 12, 2> e1_vertex_map, e2_vertex_map, e3_vertex_map;
-    Eigen::Array<double, 12, 1> w_face_true;
+        // Objects declared here can be used by all tests in the test case for Foo.
+        const std::string input_file = "./integration/cube.obj";
+        Eigen::Array<double, 8, 3> Ve_true;
+        Eigen::Array<int, 12, 3> Fe_true;
+        Eigen::Array<int, 12, 1> Fa, Fb, Fc;
+        Eigen::Array<double, 12, 3> e1, e2, e3;
+        Eigen::Array<int, 12, 2> e1_vertex_map, e2_vertex_map, e3_vertex_map;
+        Eigen::Array<double, 12, 1> w_face_true;
+        Eigen::Array<double, 12, 1> L1_edge_true, L2_edge_true, L3_edge_true;
+    
+        Eigen::Array<double, 1, 3> state;
 
 };
 
 TEST_F(TestPotential, LaplacianFactor) {
-    Eigen::Array<double, 1, 3> state;
-    state << 2, 0, 0;
+    /* Eigen::Array<double, 1, 3> state; */
+    /* state << 2, 0, 0; */
     Eigen::Array<double, Eigen::Dynamic, 3> r_v;
     r_v = Ve_true.rowwise() - state;
     Eigen::Array<double, 12, 1> w_face;
     int flag = laplacian_factor(r_v, Fa, Fb, Fc, w_face);
     EXPECT_TRUE(w_face.isApprox(w_face_true, 1e-7));
+}
+
+TEST_F(TestPotential, EdgeFactor) {
+    Eigen::Array<double, Eigen::Dynamic, 3> r_v;
+    r_v = Ve_true.rowwise() - state;
+    Eigen::Array<double, 12, 1> L1_edge, L2_edge, L3_edge;
+    int flag = edge_factor(r_v, e1, e2, e3, e1_vertex_map, e2_vertex_map, e3_vertex_map,
+            L1_edge, L2_edge, L3_edge);
+    
+    EXPECT_TRUE(L1_edge.isApprox(L1_edge_true, 1e-7));
+    EXPECT_TRUE(L2_edge.isApprox(L2_edge_true, 1e-7));
+    EXPECT_TRUE(L3_edge.isApprox(L3_edge_true, 1e-7));
 }
