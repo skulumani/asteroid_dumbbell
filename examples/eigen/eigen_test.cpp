5dc1e07f9a75c036db31fb6de483f3b5b30ccca7
Shankar Kulumani <shanks.k@gmail.com>
2018-03-13 22:41:19 -0400

    add eigen reference example

--- examples/eigen/eigen_test.cpp
+++ examples/eigen/eigen_test.cpp
@@ -2,25 +2,30 @@
 #include <iostream>
 #include <Eigen/Dense>
 
-
-void eigen_reference_class(const Eigen::Ref<const Eigen::Array<double, 3, 1> >& a, Eigen::Ref<Eigen::Array<double, 3, 1> > b) {
+// a - nonwriteable, b - writeable
+void eigen_reference_class(const Eigen::Ref<const Eigen::Array<double, 1, 3> >& a, Eigen::Ref<Eigen::Array<double, 1, 3> > b) {
     b = 2 * a;
 }
 
-void eigen_reference() {
-
+void eigen_reference(const Eigen::Array<double, 1, 3>& a, Eigen::Array<double, 1, 3>& b) {
+    b = 2*a;
 }
 
-void eigen_template() {
-
+template<typename Derived> 
+void eigen_template(const Eigen::PlainObjectBase<Derived>& a, Eigen::PlainObjectBase<Derived>& b) {
+    b = 2*a;
 }
 
 int main()
 {
-    Eigen::Array<double, 3, 1> a, b, c, d;
+    Eigen::Array<double, 1, 3> a, b, c, d;
     a << 1, 2, 3;
     eigen_reference_class(a, b);
-
+    eigen_reference(a, c);
+    eigen_template(a,  d);
     std::cout << "a : \n" << a << std::endl;
     std::cout << "b: \n" << b << std::endl;
+    std::cout << "c: \n" << c << std::endl;
+    std::cout << "d: \n" << d << std::endl;
+
 }
