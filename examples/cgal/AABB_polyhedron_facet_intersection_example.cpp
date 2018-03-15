8772579d857c6e2d646b98ceee626d22ac1cca24
Shankar Kulumani <shanks.k@gmail.com>
2018-03-15 11:08:16 -0400

    AABB intersection example

--- examples/cgal/AABB_polyhedron_facet_intersection_example.cpp
+++ examples/cgal/AABB_polyhedron_facet_intersection_example.cpp
@@ -3,4 +3,23 @@
 
 #include <CGAL/Simple_cartesian.h>
 #include <CGAL/AABB_tree.h>
-#include <
+#include <CGAL/AABB_traits.h>
+#include <CGAL/Polyhedron_3.h>
+#include <CGAL/AABB_face_graph_triangle_primitive.h>
+
+typedef CGAL::Simple_cartesian<double> K;
+typedef K::Point_3 Point;
+typedef K::Plane_3 Plane;
+typedef K::Vector_3 Vector;
+typedef K::Segment_3 Segment;
+typedef K::Ray_3 Ray;
+typedef CGAL::Polyhedron_3<K> Polyhedron;
+typedef CGAL::AABB_face_graph_triangle_primitive<Polyhedron> Primitive;
+typedef CGAL::AABB_traits<K, Primitive> Traits;
+typedef CGAL::AABB_tree<Traits> Tree;
+typedef boost::optional< Tree::Intersection_and_primitive_id<Segment>::Type > Segment_intersection;
+typedef boost::optional< Tree::Intersection_and_primitive_id<Plane>::Type > Plane_intersection;
+
+int main() {
+
+}
