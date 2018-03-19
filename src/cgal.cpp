dbace2931d2ae20cd0c03dc1854a51af904b4111
Shankar Kulumani <shanks.k@gmail.com>
2018-03-19 16:45:46 -0400

    add todo

--- src/cgal.cpp
+++ src/cgal.cpp
@@ -1,4 +1,5 @@
 #include "cgal.hpp"
+
 // TODO Modify this to compute distance instead of doing raycasting
 void distance_to_polyhedron(Eigen::Vector3d& pt, std::shared_ptr<MeshData> mesh) {
     Tree tree(faces(mesh->polyhedron).first, faces(mesh->polyhedron).second, mesh->polyhedron);
@@ -42,30 +43,30 @@ void RayCaster::update_mesh(std::shared_ptr<MeshData> mesh) {
 }
 
 void RayCaster::castray(Eigen::Vector3d& psource, Eigen::Vector3d& ptarget) {
-    
-        Tree tree(faces(this->mesh->polyhedron).first, faces(this->mesh->polyhedron).second, this->mesh->polyhedron);
-        
-        // create a Point object
-        Point a(psource(0),psource(1),psource(2));
-        Point b(ptarget(0), ptarget(1), ptarget(2));
-        Segment segment_query(a, b);
+    // TODO Create this tree once and store with object
+    Tree tree(faces(this->mesh->polyhedron).first, faces(this->mesh->polyhedron).second, this->mesh->polyhedron);
 
-        // tests intersections with segment query
-        if(tree.do_intersect(segment_query)) {
-            std::cout << "intersection(s)" << std::endl;
-        } else {
-            std::cout << "no intersection" << std::endl;
+    // create a Point object
+    Point a(psource(0),psource(1),psource(2));
+    Point b(ptarget(0), ptarget(1), ptarget(2));
+    Segment segment_query(a, b);
+
+    // tests intersections with segment query
+    if(tree.do_intersect(segment_query)) {
+        std::cout << "intersection(s)" << std::endl;
+    } else {
+        std::cout << "no intersection" << std::endl;
+    }
+
+    std::cout << tree.number_of_intersected_primitives(segment_query) << " intersection(s)" << std::endl;
+
+
+    Segment_intersection intersection = tree.any_intersection(segment_query);
+    if(intersection) {
+        // get intersection object
+        const Point* p = boost::get<Point>(&(intersection->first));
+        if(p) {
+            std::cout << "intersection object is a point " << *p << std::endl;
         }
-        
-        std::cout << tree.number_of_intersected_primitives(segment_query) << " intersection(s)" << std::endl;
-
-
-        Segment_intersection intersection = tree.any_intersection(segment_query);
-        if(intersection) {
-            // get intersection object
-            const Point* p = boost::get<Point>(&(intersection->first));
-            if(p) {
-                std::cout << "intersection object is a point " << *p << std::endl;
-            }
-        }
+    }
 }
