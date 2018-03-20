cc33ec5b27f8bd1327144cedcac2d1a9049c72d9
Shankar Kulumani <shanks.k@gmail.com>
2018-03-19 22:48:06 -0400

    Fix the object instantiation

--- src/cgal.cpp
+++ src/cgal.cpp
@@ -33,35 +33,36 @@ void distance_to_polyhedron(Eigen::Vector3d& pt, std::shared_ptr<MeshData> mesh)
 RayCaster::RayCaster(std::shared_ptr<MeshData> mesh) {
     // assign copy of pointer to object instance
     this->mesh = mesh;
-    /* this->tree = Tree tree(faces(this->mesh->polyhedron).first, */
-    /*                        faces(this->mesh->polyhedron).second, */
-    /*                        this->mesh->polyhedron); */
+    this->tree.insert(faces(this->mesh->polyhedron).first,
+            faces(this->mesh->polyhedron).second,
+            this->mesh->polyhedron);
 }
 
 void RayCaster::update_mesh(std::shared_ptr<MeshData> mesh) {
     this->mesh = mesh;
+    this->tree.clear();
+    this->tree.insert(faces(this->mesh->polyhedron).first,
+            faces(this->mesh->polyhedron).second,
+            this->mesh->polyhedron);
 }
 
 void RayCaster::castray(Eigen::Vector3d& psource, Eigen::Vector3d& ptarget) {
-    // TODO Create this tree once and store with object
-    Tree tree(faces(this->mesh->polyhedron).first, faces(this->mesh->polyhedron).second, this->mesh->polyhedron);
-
     // create a Point object
     Point a(psource(0),psource(1),psource(2));
     Point b(ptarget(0), ptarget(1), ptarget(2));
     Segment segment_query(a, b);
 
     // tests intersections with segment query
-    if(tree.do_intersect(segment_query)) {
+    if(this->tree.do_intersect(segment_query)) {
         std::cout << "intersection(s)" << std::endl;
     } else {
         std::cout << "no intersection" << std::endl;
     }
 
-    std::cout << tree.number_of_intersected_primitives(segment_query) << " intersection(s)" << std::endl;
+    std::cout << this->tree.number_of_intersected_primitives(segment_query) << " intersection(s)" << std::endl;
 
 
-    Segment_intersection intersection = tree.any_intersection(segment_query);
+    Segment_intersection intersection = this->tree.any_intersection(segment_query);
     if(intersection) {
         // get intersection object
         const Point* p = boost::get<Point>(&(intersection->first));
