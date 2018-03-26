3b8162976670c49cc3969be5c1321deb6ed305e0
Shankar Kulumani <shanks.k@gmail.com>
2018-03-26 16:25:53 -0400

    Output to mesh

--- examples/cgal/mesh_implicit_function.cpp
+++ examples/cgal/mesh_implicit_function.cpp
@@ -1,5 +1,6 @@
 // Surface mesh from implicit function
 #include <CGAL/Surface_mesh_default_triangulation_3.h>
+#include <CGAL/IO/output_surface_facets_to_polyhedron.h>
 #include <CGAL/Complex_2_in_triangulation_3.h>
 #include <CGAL/make_surface_mesh.h>
 #include <CGAL/Implicit_surface_3.h>
@@ -10,7 +11,6 @@
 #include <CGAL/Polyhedron_3.h>
 
 typedef CGAL::Simple_cartesian<double> Kernel;
-typedef CGAL::Polyhedron_3<Kernel> Polyhedron;
 // default triangulation for surface_mesher
 typedef CGAL::Surface_mesh_default_triangulation_3 Tr;
 
@@ -21,6 +21,7 @@ typedef Tr::Geom_traits GT;
 typedef GT::Sphere_3 Sphere_3;
 typedef GT::Point_3 Point_3;
 typedef GT::FT FT;
+typedef CGAL::Polyhedron_3<GT> Polyhedron;
 
 typedef FT (*Function)(Point_3);
 
@@ -53,6 +54,6 @@ int main() {
         std::cout << vit->point() << std::endl;
     }
     Polyhedron poly;    
-    /* CGAL::ouput_surface_facets_to_polyhedron(c2t3, poly); */ 
+    CGAL::output_surface_facets_to_polyhedron(c2t3, poly); 
     return 0;
 }
