38a745f03ed539a30cb3a111a7b030958e3694db
Shankar Kulumani <shanks.k@gmail.com>
2018-03-27 15:27:32 -0400

    Add script to generate mesh data

--- extern/distmesh/generate_data.m
+++ extern/distmesh/generate_data.m
@@ -7,8 +7,11 @@ c = 3;
 fd_sphere=@(p) dsphere(p,0,0,0,0.5);
 fd_ellipsoid=@(p) p(:,1).^2/a^2+p(:,2).^2/b^2+p(:,3).^2/c^2-1;
 
-
-[v,f]=distmeshsurface(fd_sphere,@huniform,0.01,1.1*[-1,-1,-1;1,1,1]);
+[v,f]=distmeshsurface(fd_sphere,@huniform,0.025,1.1*[-1,-1,-1;1,1,1]);
+f = f - 1;
 save('sphere_distmesh.mat', 'v', 'f');
 
-[v,f]=distmeshsurface(fd_ellipsoid,@huniform,0.1,1.1 * [-a, -b, -c; a, b, c]);
+[v, f]=distmeshsurface(fd_ellipsoid,@huniform,0.2,[-a-0.1,-b-0.1,-c - 0.1; a + 0.1, b+0.1, c+0.1]);
+f = f -1;
+
+save('ellipsoid_distmesh.mat', 'v', 'f');
