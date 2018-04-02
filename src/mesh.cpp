f1648a0519d7bf87951c4807343be125849dd2e7
Shankar Kulumani <shanks.k@gmail.com>
2018-04-01 20:44:43 -0400

    Removeo commented function

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -62,39 +62,6 @@ void MeshData::build_surface_mesh() {
 
 }
 
-/* // helper functions */
-/* void eigen_to_polyhedron(const Eigen::MatrixXd &V, const Eigen::MatrixXi &F, Polyhedron &P) { */
-/*     Polyhedron_builder<HalfedgeDS> builder(V, F); */
-/*     P.delegate(builder); */
-/*     CGAL_assertion(P.is_triangle(P.halfedges_begin())); */
-/* } */
-
-// TODO Add documentation - overload the () operator
-/* template<typename HDS> */
-/* void Polyhedron_builder<HDS>::operator() (HDS &hds) { */
-
-/*     typedef typename HDS::Vertex Vertex; */
-/*     typedef typename Vertex::Point Point; */
-
-/*     // create the cgal incremental builder */
-/*     CGAL::Polyhedron_incremental_builder_3<HDS> B(hds, true); */
-/*     // initialize with #v, #f, #half-edges (optional) */
-/*     B.begin_surface(V.rows(), F.rows()); */
-
-/*     // add all of the vertices */
-/*     for (int ii = 0; ii < V.rows(); ++ii) { */
-/*         B.add_vertex(Point(V(ii, 0), V(ii, 1), V(ii, 2))); */
-/*     } */
-/*     // add all of the faces */
-/*     for (int ii = 0; ii < F.rows(); ++ii) { */
-/*         B.begin_facet(); */
-/*         B.add_vertex_to_facet(F(ii, 0)); */
-/*         B.add_vertex_to_facet(F(ii, 1)); */
-/*         B.add_vertex_to_facet(F(ii, 2)); */
-/*         B.end_facet(); */
-/*     } */
-/*     B.end_surface(); */
-/* } */
 
 
 // TODO Add documentation
