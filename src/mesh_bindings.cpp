63e04c979afcdca29360249ae004bbbb842694f0
Shankar Kulumani <shanks.k@gmail.com>
2018-04-03 15:02:12 -0400

    Update mesh bindings

--- src/mesh_bindings.cpp
+++ src/mesh_bindings.cpp
@@ -10,5 +10,7 @@ PYBIND11_MODULE(mesh_data, m) {
     m.doc() = "CGAL Mesh Class holding both Polyhedron and Surface Mesh instances";
     py::class_<MeshData>(m, "MeshData")
         .def(py::init<const Eigen::MatrixXd&, const Eigen::MatrixXi&>(), "MeshData constructor",
-             py::arg("vertices"), py::arg("faces"));
+             py::arg("vertices"), py::arg("faces"))
+        .def("get_verts", &MeshData::get_verts, "Return vertices of the mesh")
+        .def("get_faces", &MeshData::get_faces, "Return faces of the mesh");
 }
