9ba127aed536ca7478f31493b86ebb0d74a3b1dd
Shankar Kulumani <shanks.k@gmail.com>
2018-04-04 23:05:44 -0400

    Loader doesn't really work

---
+++ src/loader_bindings.cpp
@@ -0,0 +1,11 @@
+#include "loader.hpp"
+#include "mesh.hpp"
+
+#include <pybind11/eigen.h>
+#include <pybind11/pybind11.h>
+
+PYBIND11_MODULE(loader, m) {
+    m.doc() = "Load an OBJ file into a MeshData object and return";
+    pybind11::class_<Loader, std::shared_ptr<Loader>>(m, "Loader")
+        .def_static("load", &Loader::load, "Loader constructor");
+}
