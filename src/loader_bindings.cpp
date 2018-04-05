b3249e483a139f37b2b52c254ec9280a3aca411d
Shankar Kulumani <shanks.k@gmail.com>
2018-04-04 23:06:10 -0400

    Add comment

--- src/loader_bindings.cpp
+++ src/loader_bindings.cpp
@@ -4,6 +4,7 @@
 #include <pybind11/eigen.h>
 #include <pybind11/pybind11.h>
 
+// This doesn't work in python. Some issue with static functions and returning the ptr
 PYBIND11_MODULE(loader, m) {
     m.doc() = "Load an OBJ file into a MeshData object and return";
     pybind11::class_<Loader, std::shared_ptr<Loader>>(m, "Loader")
