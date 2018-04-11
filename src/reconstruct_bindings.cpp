55b2e2cb2122398d32fa75bb10b7b3371f00c440
Shankar Kulumani <shanks.k@gmail.com>
2018-04-11 11:50:48 -0400

    Setup bindings

--- src/reconstruct_bindings.cpp
+++ src/reconstruct_bindings.cpp
@@ -14,5 +14,16 @@ PYBIND11_MODULE(reconstruct, m) {
 
     pybind11::class_<ReconstructMesh, std::shared_ptr<ReconstructMesh>>(m, "ReconstructMesh")
         .def(pybind11::init<std::shared_ptr<MeshData>>(), "ReconstructMesh constructor",
-                pybind11::arg("shared_ptr to MeshData object"));
+                pybind11::arg("shared_ptr to MeshData object"))
+        .def(pybind11::init<const Eigen::Ref<const Eigen::MatrixXd> &, 
+                            const Eigen::Ref<const Eigen::MatrixXi> &,
+                            const Eigen::Ref<const Eigen::MatrixXd> &>(),
+                            "ReconstructMesh constructor from eigen/numpy arrays",
+                            pybind11::arg("vertices"), pybind11::arg("faces"), pybind11::arg("weights"))
+        .def("update", &ReconstructMesh::update, "Update the mesh by incorporating the pt into the mesh",
+                pybind11::arg("pt"), pybind11::arg("max_angle"))
+        .def("update_meshdata", &ReconstructMesh::update_meshdata, "Update the meshdata object with the new mesh")
+        .def("get_verts", &ReconstructMesh::get_verts, "Get the vertices")
+        .def("get_faces", &ReconstructMesh::get_faces, "Get the faces")
+        .def("get_weights", &ReconstructMesh::get_weights, "Get the weights of the vertices");
 }
