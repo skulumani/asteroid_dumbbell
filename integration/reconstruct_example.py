2bf48ad28b3d1912e734ff2270eb2ee34301af62
Shankar Kulumani <shanks.k@gmail.com>
2018-04-12 12:01:50 -0400

    Comparing c++ and python reconstruction

--- integration/reconstruct_example.py
+++ integration/reconstruct_example.py
@@ -10,16 +10,10 @@ import scipy.io
 
 import pdb
 
-# load data from sphere and ellipsoid
-# sphere_data = scipy.io.loadmat('./data/sphere_distmesh.mat')
-# ellipsoid_data = scipy.io.loadmat('./data/ellipsoid_distmesh.mat')
-# vs, fs = sphere_data['v'], sphere_data['f']
-# ve, fe = ellipsoid_data['v'], ellipsoid_data['f']
-
-# vs, f = wavefront.ellipsoid_mesh(0.5, 0.5, 0.5, density=20, subdivisions=1)
-# ve, fe = wavefront.ellipsoid_mesh(1, 1, 1, density=10, subdivisions=1)
 sphere= surface_mesh.SurfMesh(0.5, 0.5, 0.5, 10, 0.05, 0.5)
 vs, fs = sphere.verts(), sphere.faces()
+
+vs, fs = wavefront.read_obj('./data/shape_model/CASTALIA/castalia.obj')
 
 max_angle = wavefront.spherical_surface_area(0.5, surf_area=0.03)
 
@@ -44,8 +38,8 @@ graphics.mayavi_addPoint(mfig, pt)
 mfig_cpp = graphics.mayavi_figure()
 
 vw = np.full(vs.shape[0], (np.pi * 0.5)**2)
-mesh = mesh_data.MeshData(sphere.verts(), sphere.faces())
-rmesh = reconstruct.ReconstructMesh(sphere.verts(), sphere.faces(), vw)
+mesh = mesh_data.MeshData(vs, fs)
+rmesh = reconstruct.ReconstructMesh(vs, fs, vw)
 
 rmesh.update(pt, max_angle)
 
