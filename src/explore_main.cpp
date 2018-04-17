927c8ed3784f846fedf56bd12766511727f008b2
Shankar Kulumani <shanks.k@gmail.com>
2018-04-17 17:50:06 -0400

    Start on exploration example

--- src/explore_main.cpp
+++ src/explore_main.cpp
@@ -2,6 +2,10 @@
 #include "mesh.hpp"
 #include "reconstruct.hpp"
 #include "geodesic.hpp"
+#include "surface_mesher.hpp"
+#include "cgal.hpp"
+#include "polyhedron.hpp"
+
 #include "input_parser.hpp"
 
 #include <memory>
@@ -9,28 +13,32 @@
 
 int main(int argc, char* argv[])
 {
+    // CONSTANTS
+    // semi major axes of castalia
+    Eigen::Vector3d axes(3);
+    axes << 1.6130 / 2.0, 0.9810 / 2.0, 0.8260 / 2.0;
+    double min_angle(10), max_radius(0.03), max_distance(0.5);
+
     InputParser input(argc, argv);
     if (input.option_exists("-h")) {
         std::cout << "Kinematic only exploration with asteroid reconstruction" << std::endl;
 
     }
-    const std::string input_file input.get_command_option("-i");
+    const std::string input_file = input.get_command_option("-i");
     if (input_file.empty()) {
         std::cout << "You need to input file name!" << std::endl;
-        std::cout << "explore -i asteroid.obj" << st d::endl;
+        std::cout << "explore -i asteroid.obj" << std::endl;
         return 1;
     }
-    
+
     // initialize all the objects
     std::shared_ptr<MeshData> true_asteroid;
     true_asteroid = Loader::load(input_file);
-    
+     
     RayCaster caster(true_asteroid);
-    SurfMesh ellipsoid(a, b, c, min_angle, max_radius, max_distance);
-
-    // define the initial weight of all the vertices
-    ReconstructMesh rmesh(ellipsoid.verts(), ellipsoid.faces(), weights);
-    // load the asteroid mesh and an estimate of the asteroid
+    SurfMesh ellipsoid(axes(0), axes(1), axes(2),
+            min_angle, max_radius, max_distance);
+    ReconstructMesh rmesh(ellipsoid.verts(), ellipsoid.faces());
     // place satellite in a specific location and define view axis
     // compute the intersections using the raycaster
     // reconstruct the mesh with the estimate
