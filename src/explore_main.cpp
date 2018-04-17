b6702b89eb218ead6e054cf213354600f5a22c60
Shankar Kulumani <shanks.k@gmail.com>
2018-04-17 12:03:31 -0400

    Start on exploration code

--- src/explore_main.cpp
+++ src/explore_main.cpp
@@ -14,6 +14,22 @@ int main(int argc, char* argv[])
         std::cout << "Kinematic only exploration with asteroid reconstruction" << std::endl;
 
     }
+    const std::string input_file input.get_command_option("-i");
+    if (input_file.empty()) {
+        std::cout << "You need to input file name!" << std::endl;
+        std::cout << "explore -i asteroid.obj" << st d::endl;
+        return 1;
+    }
+    
+    // initialize all the objects
+    std::shared_ptr<MeshData> true_asteroid;
+    true_asteroid = Loader::load(input_file);
+    
+    RayCaster caster(true_asteroid);
+    SurfMesh ellipsoid(a, b, c, min_angle, max_radius, max_distance);
+
+    // define the initial weight of all the vertices
+    ReconstructMesh rmesh(ellipsoid.verts(), ellipsoid.faces(), weights);
     // load the asteroid mesh and an estimate of the asteroid
     // place satellite in a specific location and define view axis
     // compute the intersections using the raycaster
