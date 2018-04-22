e08fda5bfd4db0a6d3ca26da9ecde63ceee78c21
Shankar Kulumani <shanks.k@gmail.com>
2018-04-22 18:36:37 -0400

    HDF5 example to work on

---
+++ examples/hdf5/src/cross_platform.cpp
@@ -0,0 +1,12 @@
+/**
+    Test of cross platform read/write with Python
+
+    @author Shankar Kulumani
+    @version Date
+*/
+
+// write an array to HDF5 and read it back in C++
+//
+// Then use python to read the same file 
+//
+// Also use python to write to HDF5 and have C++ read it
