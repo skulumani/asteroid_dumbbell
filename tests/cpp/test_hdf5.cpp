676d46b7318d4037d6a29ecad886d41eb046e554
Shankar Kulumani <shanks.k@gmail.com>
2018-04-24 17:12:28 -0400

    Test of reading from file

--- tests/cpp/test_hdf5.cpp
+++ tests/cpp/test_hdf5.cpp
@@ -15,7 +15,12 @@ TEST(TestHDF5Wrapper, CreateGroupinFile) {
 
 TEST(TestHDF5Wrapper, FileDataSet) {
     HDF5::File hf_file("/tmp/test.hdf5", HDF5::File::Truncate);
-    Eigen::MatrixXd mat(1, 3);
+    Eigen::MatrixXd mat(1, 3), mat_load(1, 3);
     mat = Eigen::MatrixXd::Random(1, 3);
     hf_file.create_dataset("matrix", mat);
+
+    hf_file.read_dataset("matrix", mat_load);
+
+    ASSERT_TRUE(mat.isApprox(mat_load));
+
 }
