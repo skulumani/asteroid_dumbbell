e51f3ecb5af8dcb99a6d9b6d42de9366baebd29f
Shankar Kulumani <shanks.k@gmail.com>
2018-04-24 21:42:52 -0400

    Add test for file datasets

--- tests/cpp/test_hdf5.cpp
+++ tests/cpp/test_hdf5.cpp
@@ -25,6 +25,18 @@ TEST(TestHDF5File, FileDataSet) {
 
 }
 
+TEST(TestHDF5File, ReadDataSet) {
+    std::shared_ptr<HDF5::File> hf_file_ptr = std::make_shared<HDF5::File>("/tmp/test.hdf5", HDF5::File::Truncate);
+    Eigen::MatrixXd mat(1, 3), mat_load(1, 3);
+    mat = Eigen::MatrixXd::Random(1, 3);
+    hf_file_ptr->write("matrix", mat);
+    
+    // close the file by reset
+    hf_file_ptr.reset(new HDF5::File("/tmp/test.hdf5", HDF5::File::ReadOnly));
+    // now open and read the data into a dataset
+    HDF5::DataSet hf_dataset = hf_file_ptr->open_dataset("matrix");
+}
+
 TEST(TestHDF5Group, GroupDataSet) {
     HDF5::File hf_file("/tmp/test.hdf5", HDF5::File::Truncate);
     HDF5::Group hf_group = hf_file.create_group("group");
