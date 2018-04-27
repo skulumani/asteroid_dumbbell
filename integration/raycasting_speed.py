57c80df297a9abe763121bf2ee4d7f74e0f08a6c
Shankar Kulumani <shanks.k@gmail.com>
2018-04-26 20:05:21 -0400

    Add a function for comparing raycasting Python vs C++

---
+++ integration/raycasting_speed.py
@@ -0,0 +1,25 @@
+"""Compare speed of raycasting with Python vs. Python C++ Bindings
+
+Here we compare C++ raycasting to Python/VTK raycasting
+Author
+------
+Shankar Kulumani		GWU		skulumani@gwu.edu
+"""
+
+from point_cloud import wavefront, raycaster
+from lib import cgal, mesh_data
+import numpy as np
+
+pos = np.array([5, 0, 0])
+target = np.array([0, 0, 0])
+
+filename = './data/shape_model/ITOKAWA/itokawa_very_high.obj'
+polydata = wavefront.read_obj_to_polydata(filename)
+caster = raycaster.RayCaster(polydata, flag='obb')
+intersection = caster.castray(pos, target)
+
+v, f = wavefront.read_obj(filename)
+mesh = mesh_data.MeshData(v, f)
+caster_cpp = cgal.RayCaster(mesh)
+intersection_cpp = caster_cpp.castray(pos, target)
+
