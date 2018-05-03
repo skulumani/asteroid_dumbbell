0bfff050d7dcb9c1558a9ac13a3093ee7a72e088
Shankar Kulumani <shanks.k@gmail.com>
2018-05-03 16:40:40 -0400

    Startign on explroation simulation

--- exploration_sim.py
+++ exploration_sim.py
@@ -10,3 +10,30 @@ Author
 Shankar Kulumani		GWU		skulumani@gwu.edu
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
+
+import pdb
+import h5py
+import numpy as np
+from scipy import integrate
+
+from lib import asteroid, surface_mesh
+
+from dynamic import dummbell, eoms, controller
+from point_cloud import wavefront
+
+def initialize():
+    """Initialize all the things for the simulation
+    """
+    logger = logging.getLogger(__name__)
+    logger.info('Initialize asteroid and dumbbell objects')
+    
+    ast_param = asteroid.MeshParam(v, f)
+    ast = asteroid.Asteroid('castalia', ast_param)
+    dum = dumbbell.Dumbbell(m1=500, m2=500, l=0.003)
+    des_att_func = controller.random_sweep_attitude
+    des_tran_func = controller.inertial_fixed_state
+    AbsTol = 1e-9
+    RelTol = 1e-9
+
+    return ast, dum, des_att_func, des_tran_func, AbsTol, RelTol
+
