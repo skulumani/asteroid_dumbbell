161c442d637944e7e1b92d24cbc13f074b777e3c
Shankar Kulumani <shanks.k@gmail.com>
2018-05-21 17:16:00 -0400

    Comment out some stuff

--- src/explore_control_main.cpp
+++ src/explore_control_main.cpp
@@ -109,26 +109,28 @@ int main(int argc, char* argv[])
     // LOOP HERE
     for (int ii = 0; ii < 1; ++ii) {
         
-        // compute targets for use in caster (point at the asteroid origin)
-        target = sensor.define_target(state_ptr->get_pos(), state_ptr->get_att(), dist);    
-        // perform raycasting on the true mesh (true_asteroid pointer)
-        intersection = caster.castray(state_ptr->get_pos(), target);
+        /* // compute targets for use in caster (point at the asteroid origin) */
+        /* target = sensor.define_target(state_ptr->get_pos(), state_ptr->get_att(), dist); */    
+        /* // perform raycasting on the true mesh (true_asteroid pointer) */
+        /* intersection = caster.castray(state_ptr->get_pos(), target); */
 
-        // use this measurement to update rmesh (inside holds teh mesh estimate)
-        est_rmesh_ptr->single_update(intersection, max_angle);
-        // use the updated weight to find a new positoin to move to
-        controller.explore_asteroid(0, state_ptr, est_rmesh_ptr, est_ast_ptr);
-        new_state_ptr = controller.get_desired_state();
-        // update the state ptr with the newly calculated state
-        state_ptr->update_state(new_state_ptr);
-        // save the data (raycast interseciton, position of sat, ast estimate, targets) to hdf5
-        reconstructed_vertex_group.write(std::to_string(ii), est_rmesh_ptr->get_verts());
-        reconstructed_weight_group.write(std::to_string(ii), est_rmesh_ptr->get_weights());
-        state_group.write(std::to_string(ii), state_ptr->get_state());
-        targets_group.write(std::to_string(ii), target);
-        intersections_group.write(std::to_string(ii), intersection);
+        /* // use this measurement to update rmesh (inside holds teh mesh estimate) */
+        /* est_rmesh_ptr->single_update(intersection, max_angle); */
+        /* // use the updated weight to find a new positoin to move to */
+        /* controller.explore_asteroid(0, state_ptr, est_rmesh_ptr, est_ast_ptr); */
+        /* new_state_ptr = controller.get_desired_state(); */
+        /* // update the state ptr with the newly calculated state */
+        /* state_ptr->update_state(new_state_ptr); */
+        /* // save the data (raycast interseciton, position of sat, ast estimate, targets) to hdf5 */
+        /* reconstructed_vertex_group.write(std::to_string(ii), rmesh_ptr->get_verts()); */
+        /* reconstructed_weight_group.write(std::to_string(ii), rmesh_ptr->get_weights()); */
+        /* state_group.write(std::to_string(ii), state_ptr->get_state()); */
+        /* targets_group.write(std::to_string(ii), target); */
+        /* intersections_group.write(std::to_string(ii), intersection); */
     
     }
     
+    // make sure vertices are the same in rmesh and est_ast
+    std::cout << est_rmesh_ptr->get_verts().isApprox(est_ast_ptr->get_verts()) << std::endl; 
     return 0;
 }
