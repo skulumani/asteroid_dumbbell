c564b517fbe4902992c735dc7e29779403307e5f
Shankar Kulumani <shanks.k@gmail.com>
2018-06-01 01:00:40 -0400

    Add all volume overloaded bindings

--- src/stats_bindings.cpp
+++ src/stats_bindings.cpp
@@ -6,6 +6,9 @@
 */
 
 #include "stats.hpp"
+#include "mesh.hpp"
+#include "potential.hpp"
+#include "reconstruct.hpp"
 
 #include <pybind11/pybind11.h>
 #include <pybind11/eigen.h>
@@ -14,5 +17,10 @@ PYBIND11_MODULE(stats, m) {
     m.doc() = "Statistics for Polyhedrons in C++";
 
     m.def("volume", (double (*)(const Eigen::Ref<const Eigen::MatrixXd>&,
-                                            const Eigen::Ref<const Eigen::MatrixXi>&)) &PolyVolume::volume, "Volume of polyhedron from matrices");
+                                const Eigen::Ref<const Eigen::MatrixXi>&)) &PolyVolume::volume, "Volume of polyhedron from matrices");
+    m.def("volume", (double (*)(std::shared_ptr<const MeshData>)) &PolyVolume::volume, "Volume of a polyhedron from meshdata");
+    m.def("volume", (double (*)(std::shared_ptr<const Asteroid>)) &PolyVolume::volume, "Volume of a polyhedron from Asteroid");
+    m.def("volume", (double (*)(std::shared_ptr<const MeshParam>)) &PolyVolume::volume, "Volume of a polyhedron from MeshParam");
+    m.def("volume", (double (*)(std::shared_ptr<const ReconstructMesh>)) &PolyVolume::volume, "Volume of a polyhedron from ReconstructMesh");
+
 }
