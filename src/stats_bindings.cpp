618b2be7a5489cab7e59a781127823631cbee3f5
Shankar Kulumani <shanks.k@gmail.com>
2018-06-02 14:53:39 -0400

    Add arguments for binding

--- src/stats_bindings.cpp
+++ src/stats_bindings.cpp
@@ -17,7 +17,8 @@ PYBIND11_MODULE(stats, m) {
     m.doc() = "Statistics for Polyhedrons in C++";
 
     m.def("volume", (double (*)(const Eigen::Ref<const Eigen::MatrixXd>&,
-                                const Eigen::Ref<const Eigen::MatrixXi>&)) &PolyVolume::volume, "Volume of polyhedron from matrices");
+                                const Eigen::Ref<const Eigen::MatrixXi>&)) &PolyVolume::volume, "Volume of polyhedron from matrices",
+            pybind11::arg("vertices"), pybind11::arg("faces"));
     m.def("volume", (double (*)(std::shared_ptr<const MeshData>)) &PolyVolume::volume, "Volume of a polyhedron from meshdata");
     m.def("volume", (double (*)(std::shared_ptr<const Asteroid>)) &PolyVolume::volume, "Volume of a polyhedron from Asteroid");
     m.def("volume", (double (*)(std::shared_ptr<const MeshParam>)) &PolyVolume::volume, "Volume of a polyhedron from MeshParam");
