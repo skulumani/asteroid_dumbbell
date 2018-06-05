f809e1af3a0d812262c3dd1e3520f03912b2639f
Shankar Kulumani <shanks.k@gmail.com>
2018-06-05 17:37:53 -0400

    Add another surface mesh access type

--- include/cgal_types.hpp
+++ include/cgal_types.hpp
@@ -37,6 +37,7 @@ typedef Polyhedron::Halfedge_around_facet_circulator Halfedge_facet_circulator;
 typedef CGAL::Surface_mesh<Kernel::Point_3> Mesh;
 typedef Mesh::Vertex_index Vertex_index;
 typedef Mesh::Face_index Face_index;
+typedef Mesh::Edge_index Edge_index;
 
 // AABB typedefs
 typedef CGAL::AABB_face_graph_triangle_primitive<Polyhedron> Primitive;
