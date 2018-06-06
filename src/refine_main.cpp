d7d70e4913995401efad72979e00dde84f82cbb3
Shankar Kulumani <shanks.k@gmail.com>
2018-06-06 10:05:23 -0400

    Refine example is working

--- src/refine_main.cpp
+++ src/refine_main.cpp
@@ -8,9 +8,24 @@
 
 #include <CGAL/Polygon_mesh_processing/refine.h>
 
+#include <igl/writeOBJ.h>
+
 #include <memory>
 #include <iostream>
 #include <fstream>
+#include <string>
+
+template<class T>
+T base_name(T const & path, T const & delims = "/\\")
+{
+  return path.substr(path.find_last_of(delims) + 1);
+}
+template<class T>
+T remove_extension(T const & filename)
+{
+  typename T::size_type const p(filename.find_last_of('.'));
+  return p > 0 && p != T::npos ? filename.substr(0, p) : filename;
+}
 
 int main(int argc, char* argv[]) {
     InputParser input(argc, argv);
@@ -28,6 +43,23 @@ int main(int argc, char* argv[]) {
     
     // define a set of faces to refine
     std::vector<Face_index> faces_to_refine;
-    faces_to_refine[0] = mesh->face_descriptor[0];
-
+    faces_to_refine.push_back(mesh->face_descriptor[0]);
+    
+    // vectors to store the new faces/vertices
+    std::vector<Face_index> new_faces;
+    std::vector<Vertex_index> new_vertices;
+    CGAL::Polygon_mesh_processing::refine(
+            mesh->surface_mesh,
+            faces_to_refine, // faces(mesh->surface_mesh)
+            std::back_inserter(new_faces),
+            std::back_inserter(new_vertices),
+            CGAL::Polygon_mesh_processing::parameters::density_control_factor(4.0));
+    
+    std::cout << "Refinement added " << new_vertices.size() << " vertices." << std::endl;
+    // write to OBJ
+    std::string output_file = "/tmp/" + remove_extension(base_name(input_file)) + ".obj";
+    std::cout << "Saving to: " + output_file << std::endl;
+    
+    // take surface mesh and convert to vertices
+    igl::writeOBJ(output_file, 
 }
