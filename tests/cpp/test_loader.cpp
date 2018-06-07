9bdfec64acfa4a31c32a41da59800fe0d532b885
Shankar Kulumani <shanks.k@gmail.com>
2018-06-07 15:27:19 -0400

    Fix unit test

--- tests/cpp/test_loader.cpp
+++ tests/cpp/test_loader.cpp
@@ -7,7 +7,7 @@ TEST(LoaderTest, OBJString) {
     std::string input_filename("./integration/cube.obj");
     std::shared_ptr<MeshData> mesh;
     mesh = Loader::load(input_filename);
-    ASSERT_EQ(mesh->get_faces().rows(), 16);
+    ASSERT_EQ(mesh->get_faces().rows(), 12);
     ASSERT_EQ(mesh->get_verts().rows(), 8);
 }
 
