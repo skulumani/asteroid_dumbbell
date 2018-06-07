b04e61fff0f74cec283cacc076cbf8f92f341695
Shankar Kulumani <shanks.k@gmail.com>
2018-06-07 17:49:15 -0400

    Adding potential function tests

--- tests/cpp/test_potential.cpp
+++ tests/cpp/test_potential.cpp
@@ -29,6 +29,35 @@ TEST(TestAsteroid, CubeGravity) {
     ASSERT_NEAR(ast.get_laplace(), Ulaplace_true, 1e-7);
 }
 
+TEST(TestAsteroid, TetrahedronGravity) {
+    std::shared_ptr<MeshData> mesh_data = Loader::load("./integration/tetrahedron.obj");
+    Eigen::Matrix<double, 1, 3> state;
+    state << 1, 2, 3;
+
+    Asteroid ast("cube", mesh_data);
+    ast.polyhedron_potential(state);
+
+    const double U_true = -9.15229252142212e-09;
+    Eigen::Matrix<double, 3, 1> U_grad_true;
+    U_grad_true << 6.55438157e-10, 1.31045978e-09, 1.95837246e-09;
+    Eigen::Matrix<double, 3, 3> U_grad_mat_true;
+    U_grad_mat_true <<  5.12578025e-10, -2.82609296e-10, -4.21082987e-10,
+        -2.82609296e-10,  8.87352037e-11, -8.41551959e-10,
+        -4.21082987e-10, -8.41551959e-10, -6.01313229e-10;
+ 
+    const double Ulaplace_true = 1.1575809755193234e-25;
+
+    ASSERT_NEAR(ast.get_potential(), U_true, 1e-7);
+    ASSERT_TRUE(ast.get_acceleration().isApprox(U_grad_true, 1e-7));
+    ASSERT_TRUE(ast.get_gradient_mat().isApprox(U_grad_mat_true, 1e-7));
+    ASSERT_NEAR(ast.get_laplace(), Ulaplace_true, 1e-7);
+    
+}
+
+TEST(TestAsteroid, OctahedronGravity) {
+
+}
+
 TEST(TestAsteroid, CastaliaGravity) {
     std::shared_ptr<MeshData> mesh_data = Loader::load("./data/shape_model/CASTALIA/castalia.obj");
     Eigen::Matrix<double, 1, 3> state;
