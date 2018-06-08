fe906f4a91f47a88d50f470897f22530280b93cc
Shankar Kulumani <shanks.k@gmail.com>
2018-06-08 09:17:12 -0400

    Add tests for asteroid potential

--- tests/cpp/test_potential.cpp
+++ tests/cpp/test_potential.cpp
@@ -55,7 +55,7 @@ TEST(TestAsteroid, TetrahedronGravity) {
 }
 
 TEST(TestAsteroid, AnalyticalCube) {
-    std::shared_ptr<MeshData> mesh = Loader::load("./integration/cube.obj");
+    std::shared_ptr<MeshData> mesh = Loader::load("./data/shape_model/CUBE/cube.obj");
     Asteroid ast("cube", mesh);
     ast.set_grav_constant(6.67408e-11 * (1.0 / 1000.0) * (1.0 / 1000.0) * (1.0 / 1000.0));
     ast.set_sigma(1e6 * (1000.0 ) * (1000.0) * (1000.0));
@@ -73,6 +73,21 @@ TEST(TestAsteroid, AnalyticalCube) {
 }
 
 TEST(TestAsteroid, AnalyticalCubeHighRes) {
+    std::shared_ptr<MeshData> mesh = Loader::load("./data/shape_model/CUBE/cube_200k.obj");
+    Asteroid ast("cube", mesh);
+    ast.set_grav_constant(6.67408e-11 * (1.0 / 1000.0) * (1.0 / 1000.0) * (1.0 / 1000.0));
+    ast.set_sigma(1e6 * (1000.0 ) * (1000.0) * (1000.0));
+    Eigen::Matrix<double, 1, 3> state;
+    state << 1, 2, 3;
+    
+    Eigen::Vector3d U_grad_true;
+    U_grad_true << -1.273782722739791e-06, -2.548008881415967e-06,
+                -3.823026510474731e-06;
+
+    ast.polyhedron_potential(state);
+    
+    ASSERT_NEAR(ast.get_potential(),1.783755975e-05, 1e-6);
+    ASSERT_TRUE(ast.get_acceleration().isApprox(U_grad_true, 1e-10));
 
 }
 TEST(TestAsteroid, CastaliaGravity) {
