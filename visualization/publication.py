64b79e65592c0e296bba58af827108b3cce5ada1
Shankar Kulumani <shanks.k@gmail.com>
2018-07-04 20:15:19 -0400

    Set plots for volume and uncertainty

--- visualization/publication.py
+++ visualization/publication.py
@@ -13,8 +13,12 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 from matplotlib import colors, cm
 
+import os
+
+time_label=r'Normalized Time'
+
 def figsize(scale):
-    fig_width_pt = 483.0                            # Get this from LaTeX using \the\textwidth
+    fig_width_pt =  433.62001                           # Get this from LaTeX using \the\textwidth
     inches_per_pt = 1.0/72.27                       # Convert pt to inch
     golden_mean = (np.sqrt(5.0)-1.0)/2.0            # Aesthetic ratio (you could change this)
     fig_width = fig_width_pt*inches_per_pt*scale    # width in inches
@@ -23,7 +27,7 @@ def figsize(scale):
     return fig_size
 
 def scale_figsize(wscale, hscale):
-    fig_width_pt = 483.0
+    fig_width_pt = 433.62001
     fig_height_pt = 682.0
     inches_per_pt = 1 / 72.27
     fig_width = fig_width_pt * inches_per_pt * wscale
@@ -38,50 +42,43 @@ pgf_with_latex = {                      # setup matplotlib to use latex for outp
         "font.serif": [],                   # blank entries should cause plots to inherit fonts from the document
         "font.sans-serif": [],
         "font.monospace": [],
-        "axes.labelsize": 8,               # LaTeX default is 10pt font.
-        "font.size": 8,
-        "legend.fontsize": 8,               # Make the legend/label fonts a little smaller
-        "xtick.labelsize": 8,
-        "ytick.labelsize": 8,
-        "figure.figsize": figsize(0.9),     # default fig size of 0.9 textwidth
+        # "axes.labelsize": 8,               # LaTeX default is 10pt font.
+        # "font.size": 8,
+        # "legend.fontsize": 8,               # Make the legend/label fonts a little smaller
+        # "xtick.labelsize": 8,
+        # "ytick.labelsize": 8,
+        "figure.figsize": figsize(1),     # default fig size of 0.9 textwidth
         "figure.autolayout": True,
         "pgf.preamble": [
-            r"\usepackage[utf8x]{inputenc}",    # use utf8 fonts becasue your computer can handle it :)
-            r"\usepackage[T1]{fontenc}",        # plots will be generated using this preamble
             r"\usepackage{siunitx}",
             ]
         }
 
-# matplotlib.rcParams.update(pgf_with_latex)
+matplotlib.rcParams.update(pgf_with_latex)
 sns.set_style('whitegrid', pgf_with_latex)
 sns.color_palette('bright')
-time_label = r'Normalized Time'
-linewidth=1
+linewidth=3
 
-def plot_uncertainty(time, uncertainty,fname_suffix="", wscale=1, hscale=0.75,
+def plot_uncertainty(time, uncertainty,img_path="/tmp", fname_suffix="", wscale=1, hscale=0.75,
                      pgf_save=False):
     """Plot uncertainty vs. time
     """
 
     # create a figure
-    uncertainty_figure, uncertainty_ax = plt.subplots(1, 1, figsize=scale_figsize(wscale, hscale))
+    uncertainty_figure, uncertainty_ax = plt.subplots(1, 1, figsize=figsize(scale=wscale))
     uncertainty_ax.plot(time/time[-1], uncertainty/uncertainty[0], linewidth=linewidth, linestyle='-', label='Uncertainty')
     uncertainty_ax.set_xlabel(time_label)
     uncertainty_ax.set_ylabel(r'Normalized Uncertainty')
-
-    uncertainty_unnorm_figure, uncertainty_unnorm_ax = plt.subplots(1, 1, figsize=scale_figsize(wscale, hscale))
-    uncertainty_unnorm_ax.plot(time/time[-1], uncertainty, linewidth=linewidth, linestyle='-', label='Uncertainty')
-    uncertainty_unnorm_ax.set_xlabel(time_label)
-    uncertainty_unnorm_ax.set_ylabel(r'Normalized Uncertainty')
+    uncertainty_ax.set_xlim([0, 1])
 
     # save
     if pgf_save:
         fig_handles = (uncertainty_figure,)
-        fig_names = ('uncertainty')
+        fig_names = ('uncertainty',)
         for fig, fname in zip(fig_handles, fig_names):
             plt.figure(fig.number)
-            plt.savefig(fname + '_' + fname_suffix + '.pgf')
-            plt.savefig(fname + '_' + fname_suffix + '.eps', dpi=1200)
+            plt.savefig(os.path.join(img_path, fname) +  '.pgf')
+            plt.savefig(os.path.join(img_path, fname) +  '.eps', dpi=1200)
 
     plt.show()
 
@@ -127,16 +124,32 @@ def plot_state(time, pos_inertial, pos_asteroid, fname_suffix="", wscale=1, hsca
     pos_radius_ax.set_ylabel(r'Radius')
     plt.show()
 
-def plot_volume(time, volume, true_volume, fname_suffix="", wscale=1, hscale=0.75):
+def plot_volume(time, volume, true_volume, img_path="/tmp", fname_suffix="", wscale=1, hscale=0.75, pgf_save=False):
     """Plot the volume over time"""
     
-    vol_fig, vol_ax = plt.subplots(1, 1, figsize=scale_figsize(wscale, hscale))
-    vol_ax.plot(time/time[-1], volume, linewidth=linewidth, label='Estimate',
+    vol_fig, vol_ax = plt.subplots(1, 1, figsize=figsize(scale=wscale))
+    vol_ax.plot(time/time[-1], (volume-true_volume)/true_volume,
+                linewidth=linewidth, label='Percent Error',
                 linestyle='-')
-    vol_ax.plot(time/time[-1], np.full_like(volume, true_volume), linewidth=linewidth,
-                label='Truth', linestyle='-')
+    # vol_ax.plot(time/time[-1], volume,
+    #             linewidth=linewidth, label='Estimate',
+    #             linestyle='-')
+    # vol_ax.plot(time/time[-1], np.full_like(time, true_volume),
+    #             linewidth=linewidth, label='Truth',
+    #             linestyle='-')
     vol_ax.set_xlabel(time_label)
-    vol_ax.set_ylabel(r'Volume $km^3$')
+    vol_ax.set_ylabel(r'Volume Percent Error')
+    vol_ax.set_xlim([0, 1])
+    # vol_ax.legend()
+
+    # save
+    if pgf_save:
+        fig_handles = (vol_fig,)
+        fig_names = ('volume',)
+        for fig, fname in zip(fig_handles, fig_names):
+            plt.figure(fig.number)
+            plt.savefig(os.path.join(img_path, fname) +  '.pgf')
+            plt.savefig(os.path.join(img_path, fname) +  '.eps', dpi=1200)
 
     plt.show()
 
