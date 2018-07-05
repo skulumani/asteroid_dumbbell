44ccd5c067c0617b3a191b056f04afb108944789
Shankar Kulumani <shanks.k@gmail.com>
2018-07-04 21:14:05 -0400

    Generate the data in the script as well

--- dissertation/remesh_plots.sh
+++ dissertation/remesh_plots.sh
@@ -8,7 +8,7 @@ echo "Going to generate data and the plots"
 for ast in $ASTEROIDS
 do 
     echo "Saving data to $DATA_PATH/$ast.hdf5"
-    # ./bin/explore -o ${DATA_PATH}/$ast.hdf5 -n $ast
+    ./bin/explore -o ${DATA_PATH}/$ast.hdf5 -n $ast
     echo "Now plotting $ast"
     mkdir -p $1/$ast
     PYTHONPATH=./ python dissertation/explore_plots.py -r ${DATA_PATH}/$ast.hdf5 $DATA_PATH/$ast
