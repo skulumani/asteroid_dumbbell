a74f53098a03de80b8de666dccf139f0a7e75897
Shankar Kulumani <shanks.k@gmail.com>
2018-07-05 12:15:49 -0400

    Save plots from bash script

--- dissertation/exploration_plots.sh
+++ dissertation/exploration_plots.sh
@@ -10,11 +10,25 @@ PYTHONPATH=./ python exploration_sim.py -r $IMG_PATH/castalia ./data/exploration
 PYTHONPATH=./ python exploration_sim.py -u $IMG_PATH/castalia ./data/exploration/explore/20180615_exploration_castalia_15000.hdf5 castalia
 PYTHONPATH=./ python exploration_sim.py -v $IMG_PATH/castalia ./data/exploration/explore/20180615_exploration_castalia_15000.hdf5 castalia
 
-echo "Plot Castalia exploration data"
+echo "Plot 52760 exploration data"
 mkdir -p $IMG_PATH/52760
-PYTHONPATH=./ python exploration_sim.py -r $IMG_PATH/52760 ./data/exploration/explore/20180615_exploration_castalia_15000.hdf5 castalia
-PYTHONPATH=./ python exploration_sim.py -u $IMG_PATH/52760 ./data/exploration/explore/20180615_exploration_castalia_15000.hdf5 castalia
-PYTHONPATH=./ python exploration_sim.py -v $IMG_PATH/52760 ./data/exploration/explore/20180615_exploration_castalia_15000.hdf5 castalia
+PYTHONPATH=./ python exploration_sim.py -r $IMG_PATH/52760 ./data/exploration/explore/20180615_exploration_52760_15000.hdf5 52760
+PYTHONPATH=./ python exploration_sim.py -u $IMG_PATH/52760 ./data/exploration/explore/20180615_exploration_52760_15000.hdf5 52760
+PYTHONPATH=./ python exploration_sim.py -v $IMG_PATH/52760 ./data/exploration/explore/20180615_exploration_52760_15000.hdf5 52760
+
+cd $IMG_PATH/castalia
+for file in $IMG_PATH/castalia/*.eps
+do
+    epstopdf $file
+    rm -rf $file
+done
+
+cd $IMG_PATH/52760
+for file in $IMG_PATH/52760/*.eps
+do
+    epstopdf $file
+    rm -rf $file
+done
 
 
 
