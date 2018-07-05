011b315e12ed9f0e7dc273fe942a33bd738b909e
Shankar Kulumani <shanks.k@gmail.com>
2018-07-05 16:35:26 -0400

    Now generate landing images automatically

--- dissertation/exploration_plots.sh
+++ dissertation/exploration_plots.sh
@@ -9,12 +9,22 @@ mkdir -p $IMG_PATH/castalia
 PYTHONPATH=./ python exploration_sim.py -r $IMG_PATH/castalia ./data/exploration/explore/20180615_exploration_castalia_15000.hdf5 castalia
 PYTHONPATH=./ python exploration_sim.py -u $IMG_PATH/castalia ./data/exploration/explore/20180615_exploration_castalia_15000.hdf5 castalia
 PYTHONPATH=./ python exploration_sim.py -v $IMG_PATH/castalia ./data/exploration/explore/20180615_exploration_castalia_15000.hdf5 castalia
+mkdir -p $IMG_PATH/castalia/state
+PYTHONPATH=./ python exploration_sim.py -st $IMG_PATH/castalia/state ./data/exploration/explore/20180615_exploration_castalia_15000.hdf5 castalia
+mkdir -p $IMG_PATH/castalia/refine
+PYTHONPATH=./ python exploration_sim.py -rp $IMG_PATH/castalia/refine ./data/exploration/explore/20180615_exploration_castalia_15000.hdf5 castalia
+mkdir -p $IMG_PATH/castalia/land
+PYTHONPATH=./ python exploration_sim.py -lp $IMG_PATH/castalia/land ./data/exploration/refine/20180619_castalia_refinement.hdf5 castalia
 
 echo "Plot 52760 exploration data"
 mkdir -p $IMG_PATH/52760
 PYTHONPATH=./ python exploration_sim.py -r $IMG_PATH/52760 ./data/exploration/explore/20180615_exploration_52760_15000.hdf5 52760
 PYTHONPATH=./ python exploration_sim.py -u $IMG_PATH/52760 ./data/exploration/explore/20180615_exploration_52760_15000.hdf5 52760
 PYTHONPATH=./ python exploration_sim.py -v $IMG_PATH/52760 ./data/exploration/explore/20180615_exploration_52760_15000.hdf5 52760
+mkdir -p $IMG_PATH/52760/state
+PYTHONPATH=./ python exploration_sim.py -st $IMG_PATH/52760/state ./data/exploration/explore/20180615_exploration_52760_15000.hdf5 52760
+mkdir -p $IMG_PATH/52760/refine
+PYTHONPATH=./ python exploration_sim.py -rp $IMG_PATH/52760/refine ./data/exploration/explore/20180615_exploration_52760_15000.hdf5 52760
 
 cd $IMG_PATH/castalia
 for file in $IMG_PATH/castalia/*.eps
